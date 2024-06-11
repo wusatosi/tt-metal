350c1f57a8cc61fc392f14dbae54578d42360689
Colman Glagovich <cglagovich@tenstorrent.com>
2024-06-11 13:47:39 +0000

    #0: SDPA ND repro

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
@@ -9,9 +9,11 @@ from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import (
     comp_pcc,
 )
 import tt_lib
+import ttnn
 from loguru import logger
 import pytest
 from models.utility_functions import skip_for_grayskull, skip_for_wormhole_b0
+from tqdm import tqdm
 
 
 def is_watcher_enabled():
@@ -107,5 +109,87 @@ def test_sdpa_tt_with_program_cache(device, b, nh, nkv, s, d, q_chunk_size, k_ch
 
     for _ in range(2):
         run_test_sdpa_tt(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype)
+
+    assert device.num_program_cache_entries() == 1
+
+
+def run_test_sdpa_tt_ND(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype):
+    torch.manual_seed(1234)
+
+    program_config = tt_lib.operations.primary.transformers.SDPAMultiCoreProgramConfig(
+        # compute_with_storage_grid_size=device.compute_with_storage_grid_size(),
+        compute_with_storage_grid_size=[1, 1],
+        q_chunk_size=q_chunk_size,
+        k_chunk_size=k_chunk_size,
+    )
+
+    Q = torch.randn(b, nh, s, d)
+    K = torch.randn(b, nkv, s, d)
+    V = torch.randn(b, nkv, s, d)
+    attn_mask = torch.full((s, s), torch.finfo(torch.float32).min)
+    attn_mask = torch.triu(attn_mask, diagonal=1).expand(b, 1, -1, -1)
+
+    # Print shapes of all inputs along with input names
+    logger.debug(f"Q: {Q.shape}")
+    logger.debug(f"K: {K.shape}")
+    logger.debug(f"V: {V.shape}")
+    logger.debug(f"attn_mask: {attn_mask.shape}")
+    gt = torch.nn.functional.scaled_dot_product_attention(Q, K, V, attn_mask, is_causal=False)
+
+    dram_memcfg = ttnn.experimental.tensor.MemoryConfig(
+        ttnn.experimental.tensor.TensorMemoryLayout.INTERLEAVED, ttnn.experimental.tensor.BufferType.DRAM
+    )
+
+    # tt_Q = tt_lib.tensor.Tensor(Q, dtype).to(tt_lib.tensor.Layout.TILE).to(device)
+    tt_Q = ttnn.as_tensor(Q, dtype=dtype, device=device, layout=tt_lib.tensor.Layout.TILE, memory_config=dram_memcfg)
+    # tt_K = tt_lib.tensor.Tensor(K, dtype).to(tt_lib.tensor.Layout.TILE).to(device)
+    tt_K = ttnn.as_tensor(K, dtype=dtype, device=device, layout=tt_lib.tensor.Layout.TILE, memory_config=dram_memcfg)
+    # tt_V = tt_lib.tensor.Tensor(V, dtype).to(tt_lib.tensor.Layout.TILE).to(device)
+    tt_V = ttnn.as_tensor(V, dtype=dtype, device=device, layout=tt_lib.tensor.Layout.TILE, memory_config=dram_memcfg)
+    # tt_attn_mask = tt_lib.tensor.Tensor(attn_mask, dtype).to(tt_lib.tensor.Layout.TILE).to(device)
+    tt_attn_mask = ttnn.as_tensor(
+        attn_mask, dtype=dtype, device=device, layout=tt_lib.tensor.Layout.TILE, memory_config=dram_memcfg
+    )
+
+    diff_pccs = []
+    for idx in tqdm(range(100000)):
+        tt_back = tt_lib.operations.primary.transformers.scaled_dot_product_attention(
+            tt_Q, tt_K, tt_V, tt_attn_mask, is_causal=True, program_config=program_config
+        )
+        tt_back = tt_back.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+
+        out_pass, out_pcc = comp_pcc(gt, tt_back, 0.994)
+        if idx == 0:
+            expected_pcc = out_pcc
+            print(f"First iteration PCC: {out_pcc}")
+        else:
+            if out_pcc != expected_pcc:
+                diff_pccs.append(out_pcc)
+                logger.info(f"failed ND PCC check on iter {idx}: {out_pcc}")
+        # logger.debug(f"python vs pytorch: {out_pcc}")
+        # assert out_pass
+
+    print("\n\nDifferent PCCs")
+    print("\n".join(diff_pccs))
+
+
+@skip_for_grayskull("Unsupported in GS since L1 runs OOM with most configs")
+@pytest.mark.parametrize("dtype", [tt_lib.tensor.DataType.BFLOAT16], ids=["bf16"])
+@pytest.mark.parametrize("q_chunk_size", [32], ids=["q128"])
+@pytest.mark.parametrize("k_chunk_size", [64], ids=["k128"])
+@pytest.mark.parametrize(
+    "b, nh, nkv, s, d",
+    (
+        [1, 1, 1, 512, 128],
+        # [1, 8, 1, 2048, 128],  # Llama2-70B
+        # [1, 16, 1, 2048, 64],  # Falcon-40B
+        # [1, 71, 1, 2048, 64],  # Falcon-7B
+    ),
+)
+def test_sdpa_nd_program_cache(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype, use_program_cache):
+    if (s % q_chunk_size != 0) or (s % k_chunk_size != 0):
+        pytest.skip("s must be divisible by q_chunk_size and k_chunk_size")
+
+    run_test_sdpa_tt_ND(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype)
 
     assert device.num_program_cache_entries() == 1
