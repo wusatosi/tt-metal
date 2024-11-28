794336af9d8d849d215d53c48b460d8a6b0210ec
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2024-11-28 15:43:44 -0500

    #15337: Fix incorrectly sized cb in remote cb microbenchmark (#15506)
    Force-merged by Raymond to unblock P0 from MLIR team https://github.com/tenstorrent/tt-metal/issues/15510
    
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/15510
    
    ### Problem description
    Output cb was wrong sized and exceeds actual L1 buffer size on a bank
    for remote cb test.
    Problem with validation if user/op plans to update both the dynamic
    buffer and the total cb size, in that updating only one individually
    would compare to the old value of the other and potentially fail
    assertion.
    
    ### What's changed
    Fix size calculation in test.
    Add a new api that updates both the global buffer and the total size at
    the same time, so that it can perform validation on both new values.
    
    ### Checklist
    - [x] Post commit CI passes
    - [ ] Blackhole Post commit (if applicable)
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    (cherry picked from commit 5af64271f243af41da88457cdd4a659553a6af72)

--- tt_metal/host_api.hpp
+++ tt_metal/host_api.hpp
@@ -208,8 +208,8 @@ CBHandle CreateCircularBuffer(
 const CircularBufferConfig& GetCircularBufferConfig(Program& program, CBHandle cb_handle);
 
 /**
- * Update the total size of the circular buffer at the given circular buffer handle. Updating a program-local circular
- * buffer requires all circular buffers in the program to be reallocated.
+ * Update the total size of the circular buffer at the given circular buffer handle. Updating a program-local circular buffer requires all circular buffers in the program to be reallocated.
+ * If it is required to update the address and total size of a dynamic circular buffer, use `UpdateDynamicCircularBufferAddressAndTotalSize`.
  *
  * Return value: void
  *
@@ -241,6 +241,7 @@ void UpdateCircularBufferPageSize(Program& program, CBHandle cb_handle, uint8_t 
 
 /**
  * Update the address of a dynamic circular buffer. Dynamic circular buffers share the same address space as L1 buffers.
+ * If it is required to update the address and total size of a dynamic circular buffer, use `UpdateDynamicCircularBufferAddressAndTotalSize`.
  *
  * Return value: void
  *
@@ -253,6 +254,20 @@ void UpdateCircularBufferPageSize(Program& program, CBHandle cb_handle, uint8_t 
  * address space of circular buffer `cb_handle` | const Buffer &               | L1 buffer   | Yes      |
  */
 void UpdateDynamicCircularBufferAddress(Program& program, CBHandle cb_handle, const Buffer& buffer);
+
+/**
+ * Update the address and total size of a dynamic circular buffer. Dynamic circular buffers share the same address space as L1 buffers.
+ *
+ * Return value: void
+ *
+ * | Argument   | Description                                                                              | Type                         | Valid Range | Required |
+ * |------------|------------------------------------------------------------------------------------------|------------------------------|-------------|----------|
+ * | program    | The program containing the circular buffer                                               | Program &                    |             | Yes      |
+ * | cb_handle  | ID of the circular buffer, returned by `CreateCircularBuffers`                           | CBHandle (uintptr_t) |       | Yes         |          |
+ * | buffer     | Dynamically allocated L1 buffer that shares address space of circular buffer `cb_handle` | const Buffer &               | L1 buffer   | Yes      |
+ * | total_size | New size of the circular buffer in bytes                                                 | uint32_t                     |             | Yes      |
+ */
+void UpdateDynamicCircularBufferAddressAndTotalSize(Program& program, CBHandle cb_handle, const Buffer& buffer, uint32_t total_size);
 
 /**
  * Initializes semaphore on all cores within core range (inclusive). Each core can have up to eight 4B semaphores
