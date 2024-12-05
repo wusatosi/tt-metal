4d8bfcd981c322944e3cfbe4a8c77781ddbf7933
Armin Ale <arminale@tenstorrent.com>
2024-12-05 21:23:38 +0000

    #0: better IPC

--- tests/sweep_framework/sweeps_runner.py
+++ tests/sweep_framework/sweeps_runner.py
@@ -6,14 +6,16 @@ import argparse
 import sys
 import pathlib
 import importlib
-import datetime
+import sys
+import time
 import os
 import enlighten
 import numpy as np
 import json
 from tt_metal.tools.profiler.process_ops_logs import get_device_data_generate_report
 from tt_metal.tools.profiler.common import PROFILER_LOGS_DIR
-from multiprocessing import Process, Queue
+from multiprocessing import Process
+from faster_fifo import Queue
 from queue import Empty
 import subprocess
 from framework.statuses import TestStatus, VectorValidity, VectorStatus
@@ -48,9 +50,6 @@ def get_devices(test_module):
 
 
 def gather_single_test_perf(device, test_passed, chunk_size=1):
-    if not isinstance(device, ttnn.Device):
-        logger.error("Multi-device perf is not supported. Failing.")
-        return None
     ttnn.DumpDeviceProfiler(device)
     opPerfData = get_device_data_generate_report(
         PROFILER_LOGS_DIR, None, None, None, export_csv=False, cleanup_device_log=True
@@ -99,6 +98,7 @@ def gather_single_test_perf(device, test_passed, chunk_size=1):
 
 def run(test_module, input_queue, output_queue):
     device_generator = get_devices(test_module)
+    chunk_size = 100
     try:
         device, device_name = next(device_generator)
         logger.info(f"Opened device configuration, {device_name}.")
@@ -107,40 +107,38 @@ def run(test_module, input_queue, output_queue):
         return
     try:
         while True:
-            test_vectors = input_queue.get(block=True, timeout=1)
-            test_vectors = [deserialize_vector(vec) for vec in test_vectors]
-            if len(test_vectors) > 1:
+            test_vectors = input_queue.get(block=True, timeout=4)
+            results_sent = 0
+            for i, v in enumerate(test_vectors):
+                v = deserialize_vector(v)
                 try:
-                    # try chunked first:
-                    results = test_module.run_chunk(test_vectors, device)
-                    perf_results = gather_single_test_perf(device, True, len(test_vectors))
-                    e2e_perf = None
-                    results = [result + [e2e_perf, perf_result] for result, perf_result in zip(results, perf_results)]
-                    print("Batched run passed")
+                    status, message = test_module.run(**v, device=device)
                 except Exception as e:
-                    print("Batched run failed. Trying one by one")
-                    # retry one by one:
-                    _ = gather_single_test_perf(device, False, len(test_vectors))  # clean up the previous profiles
-                    results = [None] * len(test_vectors)
-                    for i, test_vector in enumerate(test_vectors):
-                        try:
-                            status, message = test_module.run(**test_vector, device=device)
-                        except Exception as e:
-                            status, message = False, str(e)
-                        e2e_perf = None
-                        perf_result = gather_single_test_perf(device, status, chunk_size=1)
-                        results[i] = [status, message, e2e_perf, perf_result]
-            else:
-                results = [None] * len(test_vectors)
-                for i, test_vector in enumerate(test_vectors):
-                    try:
-                        status, message = test_module.run(**test_vector, device=device)
-                    except Exception as e:
-                        status, message = False, str(e)
-                    e2e_perf = None
+                    status, message = False, str(e)
+                e2e_perf = None
+                if chunk_size == 1:
                     perf_result = gather_single_test_perf(device, status, chunk_size=1)
-                    results[i] = [status, message, e2e_perf, perf_result]
-            output_queue.put(results)
+                    result = [status, message, e2e_perf, perf_result]
+                    output_queue.put(result)
+                    results_sent += 1
+                elif (i + 1 == results_sent + chunk_size or i + 1 == len(test_vectors)) and status:
+                    current_chunk_size = i - results_sent + 1
+                    perf_results = gather_single_test_perf(device, status, chunk_size=current_chunk_size)
+                    results = [[True, "", e2e_perf, perf_result] for perf_result in perf_results]
+                    output_queue.put_many(results)
+                    results_sent += current_chunk_size
+                elif not status:
+                    current_chunk_size = i - results_sent + 1
+                    if current_chunk_size == 1:
+                        # first test in chunk failed
+                        output_queue.put([status, message, e2e_perf, None])
+                    else:
+                        # previous tests in chunk passed
+                        perf_results = gather_single_test_perf(device, True, chunk_size=current_chunk_size - 1)
+                        results = [[True, "", e2e_perf, perf_result] for perf_result in perf_results]
+                        results.append([status, message, e2e_perf, None])
+                        output_queue.put_many(results)
+                    results_sent += current_chunk_size
     except Empty as e:
         try:
             # Run teardown in mesh_device_fixture
@@ -157,90 +155,82 @@ def get_timeout(test_module):
     return timeout
 
 
+def send_test_vectors(test_vectors, input_queue: Queue, start_idx=0):
+    input_queue.put(test_vectors[start_idx:])
+
+
+def proccess_responses(responses):
+    results = []
+    for response in responses:
+        result = {}
+        status, message, e2e_perf, device_perf = response[0], response[1], response[2], response[3]
+        if status and MEASURE_DEVICE_PERF and device_perf is None:
+            result["status"] = TestStatus.FAIL_UNSUPPORTED_DEVICE_PERF
+            result["message"] = message
+        elif status and MEASURE_DEVICE_PERF:
+            result["status"] = TestStatus.PASS
+            result["message"] = message
+            result["device_perf"] = device_perf
+        elif status:
+            result["status"] = TestStatus.PASS
+            result["message"] = message
+        else:
+            if "DEVICE EXCEPTION" in message:
+                logger.error(
+                    "DEVICE EXCEPTION: Device could not be initialized. The following assertion was thrown: " + message,
+                )
+                logger.info("Skipping test suite because of device error, proceeding...")
+                return []
+            if "Out of Memory: Not enough space to allocate" in message:
+                result["status"] = TestStatus.FAIL_L1_OUT_OF_MEM
+            elif "Watcher" in message:
+                result["status"] = TestStatus.FAIL_WATCHER
+            else:
+                result["status"] = TestStatus.FAIL_ASSERT_EXCEPTION
+            result["exception"] = message
+        result["e2e_perf"] = None
+        results.append(result)
+    return results, len(results)
+
+
 def execute_suite(test_module, test_vectors, pbar_manager, suite_name):
     results = []
-    input_queue = Queue()
+    input_queue = Queue(sys.getsizeof(test_vectors) * sys.getsizeof(test_vectors[0]) * 10)
     output_queue = Queue()
     p = None
     timeout = get_timeout(test_module)
-    # result tabulation code doesn't work if chunk is set to 1
-    chunk_size = 1
-    suite_pbar = pbar_manager.counter(total=len(test_vectors), desc=f"Suite: {suite_name}", leave=False)
-    for i in range(0, len(test_vectors), chunk_size):
-        if i + chunk_size < len(test_vectors):
-            actual_chunk_size = chunk_size
-        else:
-            actual_chunk_size = len(test_vectors) - i
-        vec_chunk = test_vectors[i : i + actual_chunk_size]
-        if DRY_RUN:
-            print(f"Would have executed test for vectors {json.dumps(vec_chunk, indent=2)}")
-            continue
-        result_chunk = [{} for v in vec_chunk]
-        for test_vector, result in zip(vec_chunk, result_chunk):
-            if deserialize(test_vector["validity"]) == VectorValidity.INVALID:
-                result["status"] = TestStatus.NOT_RUN
-                result["exception"] = "INVALID VECTOR: " + test_vector["invalid_reason"]
-                result["e2e_perf"] = None
-            else:
-                test_vector.pop("invalid_reason")
-                test_vector.pop("status")
-                test_vector.pop("validity")
 
-        if p is None and len(test_vectors) > 1:
+    suite_pbar = pbar_manager.counter(total=len(test_vectors), desc=f"Suite: {suite_name}", leave=False)
+    results_received = 0
+
+    if DRY_RUN:
+        print(f"Would have executed test for vectors {json.dumps(test_vectors, indent=2)}")
+        suite_pbar.close()
+        return results
+
+    while results_received < len(test_vectors):
+        if p is None:
             p = Process(target=run, args=(test_module, input_queue, output_queue))
             p.start()
+            send_test_vectors(test_vectors, input_queue, results_received)
+
         try:
-            if MEASURE_PERF:
-                # E2E performance is not meaningful when we have batched execution
-                raise NotImplementedError
-            input_queue.put(vec_chunk)
-            responses = output_queue.get(block=True, timeout=timeout)
-            for response, result in zip(responses, result_chunk):
-                status, message, e2e_perf, device_perf = response[0], response[1], response[2], response[3]
-                if status and MEASURE_DEVICE_PERF and device_perf is None:
-                    result["status"] = TestStatus.FAIL_UNSUPPORTED_DEVICE_PERF
-                    result["message"] = message
-                elif status and MEASURE_DEVICE_PERF:
-                    result["status"] = TestStatus.PASS
-                    result["message"] = message
-                    result["device_perf"] = device_perf
-                elif status:
-                    result["status"] = TestStatus.PASS
-                    result["message"] = message
-                else:
-                    if "DEVICE EXCEPTION" in message:
-                        logger.error(
-                            "DEVICE EXCEPTION: Device could not be initialized. The following assertion was thrown: "
-                            + message,
-                        )
-                        logger.info("Skipping test suite because of device error, proceeding...")
-                        return []
-                    if "Out of Memory: Not enough space to allocate" in message:
-                        result["status"] = TestStatus.FAIL_L1_OUT_OF_MEM
-                    elif "Watcher" in message:
-                        result["status"] = TestStatus.FAIL_WATCHER
-                    else:
-                        result["status"] = TestStatus.FAIL_ASSERT_EXCEPTION
-                    result["exception"] = message
-                result["e2e_perf"] = None
-                result["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                result["host"] = get_hostname()
-                result["user"] = get_username()
-        except Empty as e:
+            responses = output_queue.get_many(block=True, timeout=timeout)
+        except Empty:
             logger.warning(f"TEST TIMED OUT, Killing child process {p.pid} and running tt-smi...")
             p.terminate()
             p = None
-            result["status"], result["exception"] = TestStatus.FAIL_CRASH_HANG, "TEST TIMED OUT (CRASH / HANG)"
-            result["e2e_perf"] = None
             try:
                 tt_smi_util.run_tt_smi(ARCH, CARD_ID)
+                continue
             except:
                 logger.warning("tt-smi failed... exiting")
                 break
-
-        for _ in range(actual_chunk_size):
+        new_results, num_new_results = proccess_responses(responses)
+        results.extend(new_results)
+        results_received += num_new_results
+        for _ in range(num_new_results):
             suite_pbar.update()
-        results.extend(result_chunk)
 
     if p is not None:
         p.join()
@@ -252,12 +242,16 @@ def execute_suite(test_module, test_vectors, pbar_manager, suite_name):
 def sanitize_inputs(test_vectors):
     info_field_names = ["sweep_name", "suite_name", "vector_id", "input_hash"]
     header_info = []
+    test_vectors = [v for v in test_vectors if deserialize(v["validity"]) == VectorValidity.VALID]
     for vector in test_vectors:
         header = dict()
         for field in info_field_names:
             header[field] = vector.pop(field)
         vector.pop("timestamp")
         vector.pop("tag")
+        vector.pop("invalid_reason")
+        vector.pop("validity")
+        vector.pop("status")
         header_info.append(header)
     return header_info, test_vectors
 
