5bbb9597b16c2ebe8d824024b2b1c1c73ce4ba6d
avoraTT <avora@tenstorrent.com>
2024-12-05 12:19:11 -0800

    Add support for hop cores in matmul1d gather_in0. Remove use of sparse cbs.

--- ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
@@ -1820,6 +1820,7 @@ operation::ProgramWithCallbacks Matmul::create_program(
                     program_config.fused_activation,
                     program_config.mcast_in0,
                     program_config.gather_in0,
+                    program_config.hop_cores,
                     this->untilize_out);
             } else if constexpr (std::is_same_v<
                                      ProgramConfigType,
