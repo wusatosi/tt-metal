056b20facbdc4c1bde855cf2d934437fda89edd3
kkannan <kkannan@tenstorrent.com>
2024-12-06 07:21:51 +0000

    pcc drop in ttnn.sum op

--- tests/ttnn/unit_tests/operations/test_sum.py
+++ tests/ttnn/unit_tests/operations/test_sum.py
@@ -11,14 +11,21 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.utility_functions import torch_random
 
 
-@pytest.mark.parametrize("batch_size", [1, 16, 1, 16])
-@pytest.mark.parametrize("h", [32, 64, 41, 37])
-@pytest.mark.parametrize("w", [32, 64, 31, 63])
-@pytest.mark.parametrize("dim", [-1, -2, (2, 1)])
-def test_sum(device, batch_size, h, w, dim):
+@pytest.mark.parametrize("batch_size", [1])
+@pytest.mark.parametrize("h", [204])
+@pytest.mark.parametrize("w", [768])
+@pytest.mark.parametrize("dim", [-1])
+@pytest.mark.parametrize("input_gen_method", [1, 2])
+def test_sum(device, batch_size, h, w, dim, input_gen_method):
     torch.manual_seed(0)
 
-    torch_input_tensor = torch_random((batch_size, h, w), -100, 100, dtype=torch.bfloat16)
+    if input_gen_method == 1:
+        torch_input_tensor = torch_random((batch_size, h, w), -100, 100, dtype=torch.bfloat16)
+    elif input_gen_method == 2:
+        torch_input_tensor = torch.rand(1, h, w)
+
+    print("torch_input_tensor.dtype", torch_input_tensor.dtype)
+
     torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=True)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
@@ -28,25 +35,42 @@ def test_sum(device, batch_size, h, w, dim):
     output_tensor = ttnn.from_device(output_tensor)
 
     output_tensor = ttnn.to_torch(output_tensor)
-    assert_with_pcc(torch_output_tensor, output_tensor)
 
+    print("output_tensor", output_tensor.shape)
 
-@pytest.mark.parametrize("batch_size", [1, 16, 1, 16])
-@pytest.mark.parametrize("h", [32, 64, 41, 37])
-@pytest.mark.parametrize("w", [32, 64, 31, 63])
-def test_sum_global(device, batch_size, h, w):
-    torch.manual_seed(0)
+    import numpy as np
 
-    torch_input_tensor = torch_random((batch_size, h, w), -100, 100, dtype=torch.bfloat16)
-    torch_output_tensor = torch.sum(torch_input_tensor)
+    torch.set_printoptions(linewidth=1000, edgeitems=10, precision=20)
 
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    print("torch_input_tensor", torch_input_tensor, "\n")
 
-    output_tensor = ttnn.sum(input_tensor)
-    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
-    output_tensor = ttnn.from_device(output_tensor)
+    print("torch_output_tensor.shape", torch_output_tensor.shape)
+    print("output_tensor.shape", output_tensor.shape)
 
-    output_tensor = ttnn.to_torch(output_tensor)
-    output_tensor = output_tensor[0, 0, 0]
+    print("=========================================")
+    print("torch_output_tensor\n", torch_output_tensor)
+    print("\noutput_tensor\n", output_tensor)
+    print("=========================================")
 
     assert_with_pcc(torch_output_tensor, output_tensor)
+
+
+# @pytest.mark.parametrize("batch_size", [1, 16, 1, 16])
+# @pytest.mark.parametrize("h", [32, 64, 41, 37])
+# @pytest.mark.parametrize("w", [32, 64, 31, 63])
+# def test_sum_global(device, batch_size, h, w):
+#     torch.manual_seed(0)
+
+#     torch_input_tensor = torch_random((batch_size, h, w), -100, 100, dtype=torch.bfloat16)
+#     torch_output_tensor = torch.sum(torch_input_tensor)
+
+#     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+#     output_tensor = ttnn.sum(input_tensor)
+#     output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+#     output_tensor = ttnn.from_device(output_tensor)
+
+#     output_tensor = ttnn.to_torch(output_tensor)
+#     output_tensor = output_tensor[0, 0, 0]
+
+#     assert_with_pcc(torch_output_tensor, output_tensor)
