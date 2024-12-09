e2c1c658ddec9704e7444cc4bb5577bed265156d
Shwetank Singh <ssingh@tenstorrent.com>
2024-12-09 08:43:39 +0000

    #0: testing

--- models/demos/segformer/tt/common.py
+++ models/demos/segformer/tt/common.py
@@ -32,11 +32,40 @@ class Conv:
         self.activation = activation
         self.groups = groups
         self.dtype = dtype
+        # output_shape = ttnn.get_conv_output_dim(
+        #     input_shape=(1, 1, 1, 1),
+        #     kernel_size=self.kernel_size,
+        #     stride=(self.conv_params[0], self.conv_params[1]),
+        #     padding=(self.conv_params[2], self.conv_params[3]),
+        # )
+        # output_shape_nhw = output_shape[0] * output_shape[1] * output_shape[1]
+        # core_count_height =
         self.shard_layout = (
             ttnn.TensorMemoryLayout.HEIGHT_SHARDED if height_sharding else ttnn.TensorMemoryLayout.BLOCK_SHARDED
         )
 
     def __call__(self, device, input_tensor):
+        batch_size = input_tensor.shape[0]
+        input_height = input_tensor.shape[1]
+        input_width = input_tensor.shape[2]
+
+        output_shape = [
+            batch_size,
+            ttnn.get_conv_output_dim(input_height, self.kernel_size[0], self.conv_params[0], self.conv_params[2]),
+            ttnn.get_conv_output_dim(input_width, self.kernel_size[1], self.conv_params[1], self.conv_params[3]),
+            self.out_channels,
+        ]
+        output_shape_nhw = output_shape[0] * output_shape[1] * output_shape[2]
+        core_count_height = output_shape_nhw // 32
+        core_count_block = (min(output_shape_nhw // 32, 8)) * (output_shape[3] // 32)
+
+        self.shard_layout = (
+            ttnn.TensorMemoryLayout.HEIGHT_SHARDED
+            if core_count_height >= core_count_block
+            else ttnn.TensorMemoryLayout.BLOCK_SHARDED
+        )
+        print(f"core_count_height --> {core_count_height}, core_count_block --> {core_count_block}")
+
         conv_config = ttnn.Conv2dConfig(
             dtype=self.dtype,
             weights_dtype=ttnn.bfloat16,
@@ -57,6 +86,16 @@ class Conv:
         if self.act_block_h is not None:
             conv_config.act_block_h_override = self.act_block_h
 
+        # print(input_tensor)
+        # print(ttnn.get_memory_config(input_tensor))
+
+        if input_tensor.shape[3] != 3:
+            input_tensor = ttnn.reshape(
+                input_tensor,
+                (1, 1, input_tensor.shape[0] * input_tensor.shape[1] * input_tensor.shape[2], input_tensor.shape[3]),
+            )
+        # print(input_tensor.shape)
+        # print(f"input shape --> {input_tensor.shape}")
         [output_tensor, _out_height, _out_width, self.weights, self.bias] = ttnn.conv2d(
             input_tensor=input_tensor,
             weight_tensor=self.weights,
@@ -67,11 +106,12 @@ class Conv:
             kernel_size=self.kernel_size,
             stride=(self.conv_params[0], self.conv_params[1]),
             padding=(self.conv_params[2], self.conv_params[3]),
-            batch_size=input_tensor.shape[0],
-            input_height=input_tensor.shape[1],
-            input_width=input_tensor.shape[2],
+            batch_size=batch_size,
+            input_height=input_height,
+            input_width=input_width,
             conv_config=conv_config,
             groups=self.groups,
         )
-
+        # print(f"output shape --> {output_tensor.shape}")
+        # print(f"output config --> {ttnn.get_memory_config(output_tensor)}")
         return output_tensor, _out_height, _out_width
