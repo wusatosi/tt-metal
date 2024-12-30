01a59f5587a3fda6fcb366ad67844b1fa6d0490a
Mohamed Bahnas <mbahnas@tenstorrent.com>
2024-12-30 05:41:14 +0000

    #0: optimize FF

--- models/experimental/functional_stable_diffusion3_5/ttnn/ttnn_gelu.py
+++ models/experimental/functional_stable_diffusion3_5/ttnn/ttnn_gelu.py
@@ -26,12 +26,30 @@ class ttnn_GELU:
         hifi2_kernel_config = ttnn.WormholeComputeKernelConfig(
             math_fidelity=ttnn.MathFidelity.HiFi2,
         )
-        hidden_states = self.proj(
+
+        mm_a_y = 8
+        mm_a_x = 8
+        mm_a_x_strategy = ttnn.ShardStrategy.BLOCK
+        mm_a_x_memory_config = ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG
+        if hidden_states.shape[-2] < 512:
+            mm_a_y = 6
+            mm_a_x_strategy = ttnn.ShardStrategy.WIDTH
+            mm_a_x_memory_config = ttnn.L1_WIDTH_SHARDED_MEMORY_CONFIG
+
+        hidden_states_pre_gelu = self.proj(
             hidden_states,
             input_tensor_b=parameters["proj"]["weight"],
             bias=parameters["proj"]["bias"],
-            memory_config=ttnn.L1_MEMORY_CONFIG,
+            # memory_config=ttnn.L1_MEMORY_CONFIG,
+            memory_config=mm_a_x_memory_config,
+            core_grid=ttnn.CoreGrid(y=mm_a_y, x=mm_a_x),
             compute_kernel_config=hifi2_kernel_config,
         )
-        hidden_states = self.gelu(hidden_states, memory_config=ttnn.L1_MEMORY_CONFIG)
+        ttnn.deallocate(hidden_states)
+        hidden_states = self.gelu(
+            hidden_states_pre_gelu, memory_config=hidden_states_pre_gelu.memory_config()
+        )  # , memory_config=ttnn.DRAM_MEMORY_CONFIG) #
+        ttnn.deallocate(hidden_states_pre_gelu)
+        hidden_states = ttnn.reallocate(hidden_states)
+
         return hidden_states
