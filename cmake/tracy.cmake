7a88c1cc468215d0a957c9e000bd0edcd110f9fd
Mo <mmemarian@tenstorrent.com>
2025-01-03 23:03:45 +0000

    Working latest tracy without tools building on ubuntu 20.04

--- cmake/tracy.cmake
+++ cmake/tracy.cmake
@@ -33,32 +33,26 @@ target_link_options(TracyClient PUBLIC -rdynamic)
 # Our current fork of tracy does not have CMake support for these subdirectories
 # Once we update, we can change this
 include(ExternalProject)
-ExternalProject_Add(
-    tracy-capture
-    SOURCE_DIR ${TRACY_HOME}/capture
-    CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=Debug -DNO_FILESELECTOR=ON -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_C_COMPILER=clang-17
-    BINARY_DIR ${PROJECT_BINARY_DIR}/tools/profiler/capture
-    STAMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_stamp
-    TMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_tmp
-    INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E copy tracy-capture ${PROJECT_BINARY_DIR}/tools/profiler/bin/capture-release
-)
-ExternalProject_Add(
-    tracy-csvexport
-    SOURCE_DIR ${TRACY_HOME}/csvexport
-    CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=Debug -DNO_FILESELECTOR=ON -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_C_COMPILER=clang-17
-    BINARY_DIR ${PROJECT_BINARY_DIR}/tools/profiler/csvexport
-    STAMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_stamp
-    TMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_tmp
-    INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E copy tracy-csvexport ${PROJECT_BINARY_DIR}/tools/profiler/bin/csvexport-release
-)
-add_custom_target(
-    tracy_tools
-    ALL
-    DEPENDS
-        tracy-csvexport
-        tracy-capture
-)
+#ExternalProject_Add(
+#tracy-capture
+#SOURCE_DIR ${TRACY_HOME}/capture
+#CMAKE_ARGS
+#-DCMAKE_BUILD_TYPE=Debug -DNO_FILESELECTOR=ON -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_C_COMPILER=clang-17
+#BINARY_DIR ${PROJECT_BINARY_DIR}/tools/profiler/capture
+#STAMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_stamp
+#TMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_tmp
+#INSTALL_COMMAND
+#${CMAKE_COMMAND} -E copy tracy-capture ${PROJECT_BINARY_DIR}/tools/profiler/bin/capture-release
+#)
+#ExternalProject_Add(
+#tracy-csvexport
+#SOURCE_DIR ${TRACY_HOME}/csvexport
+#CMAKE_ARGS
+#-DCMAKE_BUILD_TYPE=Debug -DNO_FILESELECTOR=ON -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_C_COMPILER=clang-17
+#BINARY_DIR ${PROJECT_BINARY_DIR}/tools/profiler/csvexport
+#STAMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_stamp
+#TMP_DIR ${PROJECT_BINARY_DIR}/tmp/tracy_tmp
+#INSTALL_COMMAND
+#${CMAKE_COMMAND} -E copy tracy-csvexport ${PROJECT_BINARY_DIR}/tools/profiler/bin/csvexport-release
+#)
+add_custom_target(ALL)
