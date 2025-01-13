94c67802861c2ec3c223acb5310f5a5827b89714
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-01-13 15:42:03 +0000

    Removed redundant pytest

--- tests/ttnn/unit_tests/operations/test_multiplyadd.py
+++ tests/ttnn/unit_tests/operations/test_multiplyadd.py
@@ -10,11 +10,10 @@ import ttnn
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
-@pytest.mark.parametrize("h", [32 * 64])  # broj core-ova koji ce da radi
-@pytest.mark.parametrize("w", [32 * 2])  # broj tile-ova po core-u
+@pytest.mark.parametrize("h", [32 * 64])  # number of cores that does the work
+@pytest.mark.parametrize("w", [32 * 2])  # shard width, number of tiles per core
 def test_multiplyadd(device, h, w):
     torch.manual_seed(0)
-    ttnn.set_printoptions(profile="full")
     compute_grid_size = device.compute_with_storage_grid_size()
 
     torch_input_tensor1 = torch.randn((h, w), dtype=torch.bfloat16)
