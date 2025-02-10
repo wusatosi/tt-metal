3819bf551f085744153e8ae9954e5559664353d1
Stuti Raizada <sraizada@tenstorrent.com>
2025-02-10 11:12:30 -0600

    #0: fix all reduce call

--- models/demos/llama3_subdevices/tt/llama_ccl.py
+++ models/demos/llama3_subdevices/tt/llama_ccl.py
@@ -35,7 +35,7 @@ class TT_CCL:
         if teardown_persistent_fabric:
             assert enable_persistent_fabric
 
-        self.num_cbs = 1
+        self.num_cbs = 8
         self.from_remote_semaphore_handles = []
         self.to_remote_semaphore_handles = []
         self.gather_semaphore_handles = []
@@ -61,7 +61,6 @@ class TT_CCL:
             cluster_axis=cluster_axis,
             mesh_device=self.mesh_device,
             multi_device_global_semaphore=self.gather_semaphore_handles[self.gather_sem_flag],
-            math_op=ttnn.ReduceType.Sum,
             num_links=num_links,
             memory_config=memory_config,
             topology=ttnn.Topology.Linear,
