a932323ca8648b9a40a2b15f416b9d6582d8602d
Abhinav Sarje <asarje@tenstorrent.com>
2025-02-14 20:34:53 +0000

    enable ptr-trained weights

--- tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50.py
+++ tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50.py
@@ -43,10 +43,10 @@ def test_resnet_50(
     if (device.compute_with_storage_grid_size().x, device.compute_with_storage_grid_size().y) == (8, 7):
         pytest.skip("Test is not supported on n300 (8,7) grid")
 
-    if is_blackhole() and use_pretrained_weight:
-        pytest.skip(
-            "Skipping pretrained weight test on blackhole due to PCC error: https://github.com/tenstorrent/tt-metal/issues/17558"
-        )
+    # if is_blackhole() and use_pretrained_weight:
+    #     pytest.skip(
+    #         "Skipping pretrained weight test on blackhole due to PCC error: https://github.com/tenstorrent/tt-metal/issues/17558"
+    #     )
 
     test_infra = create_test_infra(
         device,
