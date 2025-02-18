8c9f5342de618b337dd19eec332b10c3e662b37a
Nour Ardo <nardo@tenstorrent.com>
2025-02-18 21:56:27 +0000

    test tilize/untilize

--- ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_program_factory.cpp
@@ -672,12 +672,12 @@ operation::ProgramWithCallbacks untilize_with_unpadding_multi_core_interleaved(
 
     constexpr uint32_t threshold_row_block = 32;
     if (num_tiles_per_row > threshold_row_block) {
+        if (num_tiles_per_row > num_tiles_per_col) {
+            return untilize_with_unpadding_multi_core_col_interleaved(a, output, use_pack_untilize, fp32_dest_acc_en);
+        }
         if (num_tiles_per_col > threshold_row_block || num_tiles_per_row > num_tiles_per_col) {
             return untilize_with_unpadding_multi_core_block_interleaved(a, output, use_pack_untilize, fp32_dest_acc_en);
         }
-    }
-    if (num_tiles_per_row > 32 && num_tiles_per_row > num_tiles_per_col) {
-        return untilize_with_unpadding_multi_core_col_interleaved(a, output, use_pack_untilize, fp32_dest_acc_en);
     }
 
     auto [ncores, all_cores, core_range, core_range_cliff, nblocks_per_core, nblocks_per_core_cliff] =
