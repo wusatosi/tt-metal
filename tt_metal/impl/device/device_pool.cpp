37dc8495a5eba76b80fec894b68e1b33c3565729
Saad Jameel <sjameel@tenstorrent.com>
2025-03-02 23:50:59 +0000

    add n150 graph descriptor

--- tt_metal/impl/device/device_pool.cpp
+++ tt_metal/impl/device/device_pool.cpp
@@ -518,6 +518,8 @@ void DevicePool::initialize_control_plane() {
     std::string mesh_graph_descriptor;
     if (tt::Cluster::instance().get_cluster_type() == tt::ClusterType::N300) {
         mesh_graph_descriptor = "n300_mesh_graph_descriptor.yaml";
+    } else if (tt::Cluster::instance().get_cluster_type() == tt::ClusterType::N150) {
+        mesh_graph_descriptor = "n150_mesh_graph_descriptor.yaml";
     } else if (tt::Cluster::instance().get_cluster_type() == tt::ClusterType::T3K) {
         mesh_graph_descriptor = "t3k_mesh_graph_descriptor.yaml";
     } else if (tt::Cluster::instance().get_cluster_type() == tt::ClusterType::GALAXY) {
