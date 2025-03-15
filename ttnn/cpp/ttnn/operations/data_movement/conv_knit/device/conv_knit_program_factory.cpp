c2838eecf106563ec0318d4bc8c81fc27b3a81cc
ppopovic <ppopovic@tenstorrent.com>
2025-03-15 16:31:10 +0000

    #0: 5 riscv parallelisation

--- ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/conv_knit_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/conv_knit_program_factory.cpp
@@ -98,14 +98,23 @@ operation::ProgramWithCallbacks conv_knit_multi_core(
     auto cb_output = tt::tt_metal::CreateCircularBuffer(program, all_cores, out_cb_config);
     log_info(tt::LogOp, "Cb1 total size {}: CB2 total size {}", src_cb_config.total_size(), out_cb_config.total_size());
 
-    std::vector<uint32_t> num_sticks_per_riscv = split_work(num_inputs_per_core_unpadded, 2);  // NC and BR for now
-    log_info(tt::LogOp, "Num sticks per riscv: NC:{} BR:{}", num_sticks_per_riscv[0], num_sticks_per_riscv[1]);
+    std::vector<uint32_t> num_sticks_per_riscv_5 = split_work(num_inputs_per_core_unpadded, 5);
+    log_info(
+        tt::LogOp,
+        "Num sticks per riscv: NC:{} BR:{} TR0:{} TR1:{} TR2:{}",
+        num_sticks_per_riscv_5[0],
+        num_sticks_per_riscv_5[1],
+        num_sticks_per_riscv_5[2],
+        num_sticks_per_riscv_5[3],
+        num_sticks_per_riscv_5[4]);
 
     auto src_buffer = input_tensor.buffer();
     auto dst_buffer = output_tensor.buffer();
 
     uint32_t current_riscv_stick_starting_index = 0;
 
+    std::map<std::string, std::string> defines;
+    defines["FIRST_DM_KERNEL"] = "1";
     std::vector<uint32_t> kernel_compile_time_args = {
         (std::uint32_t)src_cb_index,
         (std::uint32_t)out_cb_index,
@@ -114,17 +123,17 @@ operation::ProgramWithCallbacks conv_knit_multi_core(
         input_width,
         num_output_channels,
         num_inputs_per_core_unpadded,
-        num_sticks_per_riscv[0],
+        num_sticks_per_riscv_5[0],
         current_riscv_stick_starting_index};
     tt::tt_metal::KernelHandle nc_kernel_handle = tt::tt_metal::CreateKernel(
         program,
         "ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/kernels/dataflow/"
         "reader_writer_conv_knit_move_sticks_height_sharded.cpp",
         all_cores,
-        tt::tt_metal::ReaderDataMovementConfig(kernel_compile_time_args));
+        tt::tt_metal::ReaderDataMovementConfig(kernel_compile_time_args, defines));
 
-    current_riscv_stick_starting_index += num_sticks_per_riscv[0];
-    kernel_compile_time_args[7] = num_sticks_per_riscv[1];
+    current_riscv_stick_starting_index += num_sticks_per_riscv_5[0];
+    kernel_compile_time_args[7] = num_sticks_per_riscv_5[1];
     kernel_compile_time_args[8] = current_riscv_stick_starting_index;
     tt::tt_metal::KernelHandle br_kernel_handle = tt::tt_metal::CreateKernel(
         program,
@@ -133,7 +142,30 @@ operation::ProgramWithCallbacks conv_knit_multi_core(
         all_cores,
         tt::tt_metal::WriterDataMovementConfig(kernel_compile_time_args));
 
-    current_riscv_stick_starting_index += num_sticks_per_riscv[1];
+    current_riscv_stick_starting_index += num_sticks_per_riscv_5[1];
+
+    kernel_compile_time_args[7] = num_sticks_per_riscv_5[2];           // unpack
+    kernel_compile_time_args[8] = current_riscv_stick_starting_index;  // unpack
+
+    current_riscv_stick_starting_index += num_sticks_per_riscv_5[2];
+    kernel_compile_time_args.push_back(0);
+    kernel_compile_time_args.push_back(0);
+    kernel_compile_time_args[9] = num_sticks_per_riscv_5[3];            // math
+    kernel_compile_time_args[10] = current_riscv_stick_starting_index;  // math
+
+    current_riscv_stick_starting_index += num_sticks_per_riscv_5[3];
+    kernel_compile_time_args.push_back(0);
+    kernel_compile_time_args.push_back(0);
+    kernel_compile_time_args[11] = num_sticks_per_riscv_5[4];           // pack
+    kernel_compile_time_args[12] = current_riscv_stick_starting_index;  // pack
+    current_riscv_stick_starting_index += num_sticks_per_riscv_5[4];
+
+    tt::tt_metal::KernelHandle compute_kernel_handle = tt::tt_metal::CreateKernel(
+        program,
+        "ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/kernels/dataflow/"
+        "reader_writer_conv_knit_move_sticks_height_sharded.cpp",
+        all_cores,
+        tt::tt_metal::ComputeConfig{.compile_args = kernel_compile_time_args});
 
     auto override_runtime_arguments_callback = [cb_src, cb_output](
                                                    const void* operation,
--- ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/kernels/dataflow/reader_writer_conv_knit_move_sticks_height_sharded.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/conv_knit/device/kernels/dataflow/reader_writer_conv_knit_move_sticks_height_sharded.cpp
@@ -5,6 +5,18 @@
 #include <cstdint>
 #include "tt_metal/hw/inc/utils/utils.h"
 
+#include "debug/dprint.h"
+
+#if defined(TRISC_MATH) or defined(TRISC_UNPACK) or defined(TRISC_PACK)
+#include "compute_kernel_api/matmul.h"
+#include "compute_kernel_api/pack_untilize.h"
+#include "compute_kernel_api/tile_move_copy.h"
+#endif
+
+#if defined(TRISC_UNPACK) or defined(TRISC_PACK) or defined(TRISC_MATH)
+namespace NAMESPACE {
+#endif
+
 inline __attribute__((always_inline)) constexpr uint32_t log2_constexpr(uint32_t n) {
     uint32_t p = 0;
     while (n > 1) {
@@ -14,39 +26,140 @@ inline __attribute__((always_inline)) constexpr uint32_t log2_constexpr(uint32_t
     return p;
 }
 
+#if defined(TRISC_MATH) or defined(TRISC_UNPACK) or defined(TRISC_PACK)
+void MAIN{
+#else
 void kernel_main() {
+#endif
+
     constexpr uint32_t src_cb_id = get_compile_time_arg_val(0);
-    constexpr uint32_t dst_cb_id = get_compile_time_arg_val(1);
-    constexpr uint32_t input_unit_size_in_bytes = get_compile_time_arg_val(2);  // stick size
-    constexpr uint32_t num_input_channels = get_compile_time_arg_val(3);
-    constexpr uint32_t input_width = get_compile_time_arg_val(4);
-    constexpr uint32_t num_output_channels = get_compile_time_arg_val(5);
-    constexpr uint32_t num_sticks_for_all_riscvs = get_compile_time_arg_val(6);
-    constexpr uint32_t num_sticks_for_this_riscv = get_compile_time_arg_val(7);
-    constexpr uint32_t current_riscv_stick_starting_index = get_compile_time_arg_val(8);
+constexpr uint32_t dst_cb_id = get_compile_time_arg_val(1);
+constexpr uint32_t input_unit_size_in_bytes = get_compile_time_arg_val(2);  // stick size
+constexpr uint32_t num_input_channels = get_compile_time_arg_val(3);
+constexpr uint32_t input_width = get_compile_time_arg_val(4);
+constexpr uint32_t num_output_channels = get_compile_time_arg_val(5);
+constexpr uint32_t num_sticks_for_all_riscvs = get_compile_time_arg_val(6);
 
-    // temp, fixme
-    constexpr uint32_t stick_num_elements = input_unit_size_in_bytes / 2;  // assuming hardcoded bfloat16
-    constexpr uint32_t output_width = input_width * num_input_channels / (2 * num_output_channels);
-    constexpr uint32_t num_elements_to_write_in_dst_stick = num_output_channels;
-    constexpr uint32_t half_of_input_channels = num_input_channels / 2;
-    constexpr uint32_t num_widths_done_by_other_riscvs = current_riscv_stick_starting_index / input_width;
-    constexpr bool num_elements_to_write_in_dst_stick_is_power_of_2 = is_power_of_2(num_elements_to_write_in_dst_stick);
+#if !defined(TRISC_MATH) && !defined(TRISC_PACK)
+constexpr uint32_t num_sticks_for_this_riscv = get_compile_time_arg_val(7);
+constexpr uint32_t current_riscv_stick_starting_index = get_compile_time_arg_val(8);
+#endif
 
-    volatile tt_l1_ptr uint16_t* src_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_write_ptr(src_cb_id));
-    volatile tt_l1_ptr uint16_t* dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_write_ptr(dst_cb_id));
-    src_cb_ptr += current_riscv_stick_starting_index * stick_num_elements;
-    dst_cb_ptr += num_widths_done_by_other_riscvs * output_width * stick_num_elements +
-                  current_riscv_stick_starting_index * 2 *
-                      stick_num_elements;  // move to adequate row in dst in addition to moving to the next sticks
+#if defined(TRISC_MATH)
+constexpr uint32_t num_sticks_for_this_riscv = get_compile_time_arg_val(9);
+constexpr uint32_t current_riscv_stick_starting_index = get_compile_time_arg_val(10);
+#endif
 
-    uint32_t num_input_sticks_read = current_riscv_stick_starting_index - num_widths_done_by_other_riscvs * input_width;
-    for (uint32_t i = 0; i < num_sticks_for_this_riscv; i++) {
-        uint32_t written_in_dst_stick = 0;
-        uint32_t stick_index = 0;
+#if defined(TRISC_PACK)
+constexpr uint32_t num_sticks_for_this_riscv = get_compile_time_arg_val(11);
+constexpr uint32_t current_riscv_stick_starting_index = get_compile_time_arg_val(12);
+#endif
 
-        // Copy the second half of the sticks to the destination in the row below
-        volatile tt_l1_ptr uint16_t* dst_cb_ptr_row_below = dst_cb_ptr + output_width * stick_num_elements;
+volatile tt_l1_ptr uint16_t* src_cb_ptr = nullptr;
+volatile tt_l1_ptr uint16_t* dst_cb_ptr = nullptr;
+
+#if !defined(TRISC_MATH) and !defined(TRISC_PACK) and !defined(TRISC_UNPACK)
+src_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_write_ptr(src_cb_id));
+dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_write_ptr(dst_cb_id));
+#endif
+
+// Math/pack/unpack threads do not have access to get_write_ptr, so we need to pass it:
+// DM to unpack, unpack passes it to math, and math passes it to pack
+// Do it with a handshake with math thread:
+// math sends a signal to unpack
+// unpack reads the signal and sends the pointer to math
+// math gets the signal
+// Unfortunately, we can't use random L1 address to do so, because of kernel prefetching
+// so we need to use a safe address
+// This is the safe address:
+// auto buf = GET_MAILBOX_ADDRESS_DEV(watcher.debug_ring_buf);
+// uint32_t* data = buf->data;
+// This makes the use of watcher debug ring buffer feature obsolete.
+#if defined(TRISC_MATH) or defined(TRISC_UNPACK) or defined(TRISC_PACK) or defined(FIRST_DM_KERNEL)
+auto buf = GET_MAILBOX_ADDRESS_DEV(watcher.debug_ring_buf);
+volatile uint32_t* data = (volatile uint32_t*)buf->data;
+
+constexpr uint32_t signal_dm_unp = 0x00deba01;
+constexpr uint32_t signal_unp_math_1 = 0x00baba01;
+constexpr uint32_t signal_unp_math_2 = 0x00baba02;
+constexpr uint32_t signal_math_pack_1 = 0x00deda01;
+constexpr uint32_t signal_math_pack_2 = 0x00deda02;
+#endif
+
+#if defined(FIRST_DM_KERNEL)
+while (*data != signal_dm_unp);  // wait for signal_dm_unp from unpack
+*data = (uint32_t)src_cb_ptr;    // send the pointer
+while (*data != signal_dm_unp);  // wait for receive
+*data = (uint32_t)dst_cb_ptr;    // send the pointer
+#endif
+
+#if defined(TRISC_MATH)
+while (*data != signal_unp_math_2);                                  // wait for unpack to send the pointer
+*data = signal_unp_math_1;                                           // send signal_unp_math_1 to unpack
+while (*data == signal_unp_math_1);                                  // wait for unpack to send the pointer
+src_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+*data = signal_unp_math_1;                                           // send signal_unp_math_1 to unpack
+while (*data == signal_unp_math_1);                                  // wait for unpack to send the pointer
+dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+// complete
+
+// signal pack we ready to send data
+*data = signal_math_pack_1;
+while (*data != signal_math_pack_2);  // wait for signal_math_pack_2 from pack
+*data = (uint32_t)src_cb_ptr;         // send the pointer
+while (*data != signal_math_pack_2);  // wait for receive
+*data = (uint32_t)dst_cb_ptr;         // send the pointer
+// complete
+#endif
+
+#if defined(TRISC_UNPACK)
+*data = signal_dm_unp;           // send signal_dm_unp that we are ready to accept data
+while (*data == signal_dm_unp);  // wait for dm to send the pointer
+src_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+*data = signal_dm_unp;           // send signal_dm_unp that we are ready to accept data
+while (*data == signal_dm_unp);  // wait for dm to send the pointer
+dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+// complete
+
+*data = signal_unp_math_2;           // send signal_unp_math_2 to math
+while (*data != signal_unp_math_1);  // wait for signal_unp_math_1 from math
+*data = (uint32_t)src_cb_ptr;        // write the pointer
+while (*data != signal_unp_math_1);  // wait for signal_unp_math_1 from math
+*data = (uint32_t)dst_cb_ptr;        // write the pointer
+// complete
+#endif
+
+#if defined(TRISC_PACK)
+while (*data != signal_math_pack_1);  // wait for signal_math_pack_1 from math
+*data = signal_math_pack_2;           // send signal_math_pack_2 to math that we are ready to accept data
+while (*data == signal_math_pack_2);  // wait for math to send the pointer
+src_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+*data = signal_math_pack_2;                                          // signal math we ready for next pointer
+while (*data == signal_math_pack_2);                                 // wait for math to send the pointer
+dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(*data);  // fetch the pointer
+// complete
+#endif
+
+// temp, fixme
+constexpr uint32_t stick_num_elements = input_unit_size_in_bytes / 2;  // assuming hardcoded bfloat16
+constexpr uint32_t output_width = input_width * num_input_channels / (2 * num_output_channels);
+constexpr uint32_t num_elements_to_write_in_dst_stick = num_output_channels;
+constexpr uint32_t half_of_input_channels = num_input_channels / 2;
+constexpr uint32_t num_widths_done_by_other_riscvs = current_riscv_stick_starting_index / input_width;
+constexpr bool num_elements_to_write_in_dst_stick_is_power_of_2 = is_power_of_2(num_elements_to_write_in_dst_stick);
+
+src_cb_ptr += current_riscv_stick_starting_index * stick_num_elements;
+dst_cb_ptr += num_widths_done_by_other_riscvs * output_width * stick_num_elements +
+              current_riscv_stick_starting_index * 2 *
+                  stick_num_elements;  // move to adequate row in dst in addition to moving to the next sticks
+
+uint32_t num_input_sticks_read = current_riscv_stick_starting_index - num_widths_done_by_other_riscvs * input_width;
+for (uint32_t i = 0; i < num_sticks_for_this_riscv; i++) {
+    uint32_t written_in_dst_stick = 0;
+    uint32_t stick_index = 0;
+
+    // Copy the second half of the sticks to the destination in the row below
+    volatile tt_l1_ptr uint16_t* dst_cb_ptr_row_below = dst_cb_ptr + output_width * stick_num_elements;
 
 #pragma GCC unroll 16
         for (uint32_t j = 0; j < half_of_input_channels; j++) {
@@ -74,7 +187,7 @@ void kernel_main() {
             // skip row we have just written to
             dst_cb_ptr += output_width * stick_num_elements;
         }
-    }
+}
 
     // dst_cb_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_write_ptr(dst_cb_id));
     // for (uint32_t i = 0; i < 18; i++) {
@@ -99,3 +212,8 @@ void kernel_main() {
     //         DPRINT << "ROW END" << ENDL();
     // }
 }
+
+#if defined(TRISC_UNPACK) or defined(TRISC_PACK) or defined(TRISC_MATH)
+}
+;  // namespace NAMESPACE
+#endif
