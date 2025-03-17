e1435bdc91393d81a046f1026505b0cc6fdfc5dc
Sofija <sjovic@tenstorrent.com>
2025-03-17 16:24:51 +0000

    #0: run sd in a loop

--- models/demos/wormhole/stable_diffusion/tests/test_unet_2d_condition_model.py
+++ models/demos/wormhole/stable_diffusion/tests/test_unet_2d_condition_model.py
@@ -136,40 +136,42 @@ def test_unet_2d_condition_model_512x512(device, batch_size, in_channels, input_
     reader_patterns_cache = {}
     model = UNet2D(device, parameters, batch_size, input_height, input_width, reader_patterns_cache)
 
-    first_iter = time.time()
-    use_signpost = True
-    try:
-        from tracy import signpost
-    except ModuleNotFoundError:
-        use_signpost = False
-    if use_signpost:
-        signpost(header="start")
-    ttnn_output_ = model(
-        input,
-        timestep=ttnn_timestep,
-        encoder_hidden_states=encoder_hidden_states,
-        class_labels=class_labels,
-        attention_mask=attention_mask,
-        cross_attention_kwargs=cross_attention_kwargs,
-        return_dict=return_dict,
-        config=config,
-    )
-    if use_signpost:
-        signpost(header="stop")
-    first_iter = time.time() - first_iter
-    print(f"First iteration took {first_iter} seconds")
+    # first_iter = time.time()
+    # use_signpost = True
+    # try:
+    #     from tracy import signpost
+    # except ModuleNotFoundError:
+    #     use_signpost = False
+    # if use_signpost:
+    #     signpost(header="start")
+    # ttnn_output_ = model(
+    #     input,
+    #     timestep=ttnn_timestep,
+    #     encoder_hidden_states=encoder_hidden_states,
+    #     class_labels=class_labels,
+    #     attention_mask=attention_mask,
+    #     cross_attention_kwargs=cross_attention_kwargs,
+    #     return_dict=return_dict,
+    #     config=config,
+    # )
+    # if use_signpost:
+    #     signpost(header="stop")
+    # first_iter = time.time() - first_iter
+    # print(f"First iteration took {first_iter} seconds")
 
-    second_iter = time.time()
-    ttnn_output = model(
-        input,
-        timestep=ttnn_timestep,
-        encoder_hidden_states=encoder_hidden_states,
-        class_labels=class_labels,
-        attention_mask=attention_mask,
-        cross_attention_kwargs=cross_attention_kwargs,
-        return_dict=return_dict,
-        config=config,
-    )
+    for i in range(10000):
+        print(f"Running iteration {i}")
+        second_iter = time.time()
+        ttnn_output = model(
+            input,
+            timestep=ttnn_timestep,
+            encoder_hidden_states=encoder_hidden_states,
+            class_labels=class_labels,
+            attention_mask=attention_mask,
+            cross_attention_kwargs=cross_attention_kwargs,
+            return_dict=return_dict,
+            config=config,
+        )
     second_iter = time.time() - second_iter
     print(f"Second iteration took {second_iter} seconds")
     ttnn_output = ttnn_to_torch(ttnn_output)
