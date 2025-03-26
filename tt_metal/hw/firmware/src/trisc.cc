efdfcc63bd617b370d22af4b6f59e7abf34102b4
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-03-26 16:21:17 +0000

    #0: WA7_S2

--- tt_metal/hw/firmware/src/trisc.cc
+++ tt_metal/hw/firmware/src/trisc.cc
@@ -78,7 +78,8 @@ using namespace ckernel;
 int main(int argc, char *argv[]) {
     // disable_gathering();
     // disable_branch_prediction();
-    disable_riscv_out_of_order();
+    // disable_riscv_out_of_order();
+    disable_prefetch();
     configure_l1_data_cache();
     DIRTY_STACK_MEMORY();
     WAYPOINT("I");
