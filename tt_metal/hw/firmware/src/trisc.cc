a62c553409e753c552d2f3f526315bb0ff29e83f
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-03-26 09:57:48 +0000

    #0: WA3 from matmul hang table, sheet 2 (WA3_S2 further)

--- tt_metal/hw/firmware/src/trisc.cc
+++ tt_metal/hw/firmware/src/trisc.cc
@@ -88,7 +88,10 @@ void init_sync_registers() {
 }
 
 int main(int argc, char *argv[]) {
-    disable_gathering();
+    // disable_gathering();
+    // disable_branch_prediction();
+    // disable_riscv_out_of_order();
+    // disable_prefetch();
     configure_l1_data_cache();
     DIRTY_STACK_MEMORY();
     WAYPOINT("I");
