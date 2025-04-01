7d1849ceb95c3db68e2c2c8544fb1c57bdb65a14
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-04-01 11:51:43 +0000

    #0: Disable all Blackhole-specific features: - instruction coalescing - branch prediction - out-of-order execution - instruction prefetching - L1 data-cache or low-cache

--- tt_metal/hw/inc/risc_common.h
+++ tt_metal/hw/inc/risc_common.h
@@ -186,26 +186,59 @@ inline __attribute__((always_inline)) void invalidate_l1_cache() {
 #endif
 }
 
-inline __attribute__((always_inline)) void configure_l1_data_cache() {
+inline void disable_branch_prediction() {
 #if defined(ARCH_BLACKHOLE)
-#if defined(DISABLE_L1_DATA_CACHE)
-    // Disables Blackhole's L1 cache. Grayskull and Wormhole do not have L1 cache
-    // L1 cache can be disabled by setting `TT_METAL_DISABLE_L1_DATA_CACHE_RISCVS` env var
-    // export TT_METAL_DISABLE_L1_DATA_CACHE_RISCVS=<BR,NC,TR*,ER*>
+    // Disable branch prediction
     asm(R"ASM(
-        li t1, 0x8
+        .option push
+        fence
+        li   t1, 0x2
         csrrs zero, 0x7c0, t1
-         )ASM" ::
-            : "t1");
-#elif !defined(ENABLE_HW_CACHE_INVALIDATION)
-    // Disable gathering to stop HW from invalidating the data cache after 128 transactions
-    // This is default enabled
-    asm(R"ASM(
-        lui  t1, 0x40
-        csrrs zero, 0x7c0, t1
-         )ASM" ::
+        .option pop
+            )ASM" ::
             : "t1");
 #endif
+}
+
+inline void disable_riscv_out_of_order() {
+#if defined(ARCH_BLACKHOLE)
+    // Disable branch prediction
+    asm(R"ASM(
+        .option push
+        fence
+        li   t1, 0x1
+        csrrs zero, 0x7c0, t1
+        .option pop
+            )ASM" ::
+            : "t1");
+#endif
+}
+
+inline void disable_prefetch() {
+#if defined(ARCH_BLACKHOLE)
+    // Disable branch prediction
+    asm(R"ASM(
+        .option push
+        fence
+        li   t1, 0x4
+        csrrs zero, 0x7c0, t1
+        .option pop
+            )ASM" ::
+            : "t1");
+#endif
+}
+
+inline void disable_l1_data_cache() {
+#if defined(ARCH_BLACKHOLE)
+    // Disable branch prediction
+    asm(R"ASM(
+        .option push
+        fence
+        li t1, 0x8
+        csrrs zero, 0x7c0, t1
+        .option pop
+            )ASM" ::
+            : "t1");
 #endif
 }
 
