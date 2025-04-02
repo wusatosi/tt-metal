3d4b0b58877528b225be7ee4244a984bd2cf71cf
Ilija Anastasijevic <ianastasijevic@tenstorrent.com>
2025-04-02 21:14:05 +0000

    Program factory and kernels that print

--- ttnn/cpp/ttnn/operations/examples/my_operation/device/kernels/compute/my_eltwise.cpp
+++ ttnn/cpp/ttnn/operations/examples/my_operation/device/kernels/compute/my_eltwise.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include "compute_kernel_api/common.h"
+#include "debug/dprint.h"
 
 namespace NAMESPACE {
-void MAIN {}
+void MAIN { DPRINT("Hello from my_eltwise kernel!"); }
 }  // namespace NAMESPACE
