64822b7e67549d09c5747c9a087c0121c72164fa
asaigal <asaigal@tenstorrent.com>
2025-04-03 01:50:52 +0000

    Tune falcone 7B demo perf numbers

--- models/demos/t3000/falcon7b/demo_t3000.py
+++ models/demos/t3000/falcon7b/demo_t3000.py
@@ -10,9 +10,9 @@ from models.utility_functions import is_wormhole_b0
 @pytest.mark.parametrize(
     "perf_mode, max_seq_len, expected_perf_metrics, greedy_sampling, expected_greedy_output_path",
     (
-        (True, 128, {"prefill_t/s": 10963, "decode_t/s": 3662, "decode_t/s/u": 14.3}, False, None),
-        (True, 1024, {"prefill_t/s": 12656, "decode_t/s": 3500, "decode_t/s/u": 13.3}, False, None),
-        (True, 2048, {"prefill_t/s": 10813, "decode_t/s": 3234, "decode_t/s/u": 12.6}, False, None),
+        (True, 128, {"prefill_t/s": 10963, "decode_t/s": 3800, "decode_t/s/u": 14.9}, False, None),
+        (True, 1024, {"prefill_t/s": 12656, "decode_t/s": 3426, "decode_t/s/u": 13.5}, False, None),
+        (True, 2048, {"prefill_t/s": 10813, "decode_t/s": 3218, "decode_t/s/u": 12.6}, False, None),
         (True, 128, None, False, None),
         (True, 1024, None, False, None),
         (True, 2048, None, False, None),
