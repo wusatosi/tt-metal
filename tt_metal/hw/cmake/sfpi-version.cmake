0ee7b8a4231d8d66144bf879060aac072b74e2be
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-03 14:57:30 +0000

    #0: sfpi nop

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,6 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.9.0/sfpi-x86_64-Linux.tgz"
-    "9bb1e0ef4287b9bbf71c205984842873"
+    "v6.9.0-nops/sfpi-x86_64-Linux.tgz"
+    "96c3d3fdf20b8eee5ff7116377c8f983"
 )
