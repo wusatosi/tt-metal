1ffa9ef4431ea621a9a7ca2c751d41515e2ea65d
ntarafdar <ntarafdar@tenstorrent.com>
2025-04-04 23:11:25 +0000

    DO NOT MERGE: setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export TT_METAL_ENABLE_ERISC_IRAM=1
+export GIT_EDITOR=vim
+export TT_METAL_ENV=dev
+export PYTHONPATH=`pwd`
+export TT_METAL_HOME=`pwd`
+export ARCH_NAME=wormhole_b0
+export TT_METAL_WORKER_RINGBUFFER_SIZE=122880
+git config core.commentchar ">"
