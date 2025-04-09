c95a7e1548274c7d35ee3f79ccf295a33116dc6e
Colman Glagovich <cglagovich@tenstorrent.com>
2025-04-09 19:00:50 +0000

    Generalize matmul_reduce implementation
    (cherry picked from commit 7a557c55c298ee19798660f76ec2014176559758)

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
@@ -235,7 +235,9 @@ def test_sdpa_tt_with_program_cache(device, b, nh, nkv, s, d, q_chunk_size, k_ch
     assert device.num_program_cache_entries() == 1
 
 
-def run_sdpa_noncausal(device, b, nh, nkv, sq, d, q_chunk_size, k_chunk_size, dtype, sk=None, use_mask=True):
+def run_sdpa_noncausal(
+    device, b, nh, nkv, sq, d, q_chunk_size, k_chunk_size, dtype, sk=None, use_mask=True, grid=None, mse=None
+):
     torch.manual_seed(1234)
     if sk is None:
         sk = sq
@@ -306,8 +308,11 @@ def run_sdpa_noncausal(device, b, nh, nkv, sq, d, q_chunk_size, k_chunk_size, dt
 
     out_pass, out_pcc = comp_pcc(gt, tt_back, 0.994)
     logger.debug(f"python vs pytorch: {out_pcc}")
-    logger.debug(f"mse: {((gt - tt_back) ** 2).mean()}")
+    got_mse = ((gt - tt_back) ** 2).mean()
+    logger.debug(f"mse: {got_mse}")
     assert out_pass
+    if mse is not None:
+        assert got_mse < mse
 
 
 # @pytest.mark.skip(reason="ND PCC issues")
