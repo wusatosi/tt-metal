04ed73b5f568e5baa16f98b9e593a00fbfb7d880
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-09 14:25:07 +0000

    #0: sfpu test

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,6 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.8.0/sfpi-release.tgz"
-    "fca4cea73901483f741f6f325c289c7d"
+    "v6.9.0-ldst-20204/sfpi-x86_64-Linux.tgz"
+    "f4d0a47bc0f742d9ad91ca00817e66dd"
 )
