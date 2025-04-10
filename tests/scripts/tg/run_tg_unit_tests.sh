5effa9568c23fde3518119c8ff5bb274e38c0716
Rose Li <roseli@tenstorrent.com>
2025-04-09 22:44:27 -0400

    nuke llama tests

--- tests/scripts/tg/run_tg_unit_tests.sh
+++ tests/scripts/tg/run_tg_unit_tests.sh
@@ -9,9 +9,9 @@ run_tg_llama3.1-70b_tests() {
   echo "LOG_METAL: Running run_tg_llama3.1-70b_tests"
 
   # Llama3.1-70B weights
-  llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.1-70B-Instruct/
+  #llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.1-70B-Instruct/
 
-  LLAMA_DIR=$llama70b FAKE_DEVICE=TG pytest models/demos/llama3_subdevices/tests/unit_tests ; fail+=$?
+  #LLAMA_DIR=$llama70b FAKE_DEVICE=TG pytest models/demos/llama3_subdevices/tests/unit_tests ; fail+=$?
 
   # Record the end time
   end_time=$(date +%s)
