a0f0b90bbe9dc79675457dc11204a932f6226475
Kannika Kabilar <kkabilar@tenstorrent.com>
2025-04-16 11:24:35 -0500

    comment out most tests

--- tests/scripts/run_tt_eager.py
+++ tests/scripts/run_tt_eager.py
@@ -31,18 +31,18 @@ from tests.scripts.cmdline_args import (
 
 TT_EAGER_COMMON_TEST_ENTRIES = (
     TestEntry("tt_eager/tests/ops/test_eltwise_binary_op", "ops/test_eltwise_binary_op"),
-    TestEntry("tt_eager/tests/ops/test_bcast_op", "ops/test_bcast_op"),
-    TestEntry("tt_eager/tests/ops/test_bmm_op", "ops/test_bmm_op"),
-    void_for_bh(void_for_whb0(TestEntry("tt_eager/tests/ops/test_eltwise_unary_op", "ops/test_eltwise_unary_op"))),
-    TestEntry("tt_eager/tests/ops/test_layernorm_op", "ops/test_layernorm_op"),
-    TestEntry("tt_eager/tests/ops/test_softmax_op", "ops/test_softmax_op"),
-    TestEntry(
-        "tt_eager/tests/tensors/test_host_device_loopback",
-        "tensors/test_host_device_loopback",
-    ),
-    TestEntry("tt_eager/tests/tensors/test_copy_and_move", "tensors/test_copy_and_move"),
-    TestEntry("tt_eager/tests/tensors/test_raw_host_memory_pointer", "tensors/test_raw_host_memory_pointer"),
-    TestEntry("tt_eager/tests/tensors/test_async_tensor_apis", "tensors/test_async_tensor_apis"),
+    # TestEntry("tt_eager/tests/ops/test_bcast_op", "ops/test_bcast_op"),
+    # TestEntry("tt_eager/tests/ops/test_bmm_op", "ops/test_bmm_op"),
+    # void_for_bh(void_for_whb0(TestEntry("tt_eager/tests/ops/test_eltwise_unary_op", "ops/test_eltwise_unary_op"))),
+    # TestEntry("tt_eager/tests/ops/test_layernorm_op", "ops/test_layernorm_op"),
+    # TestEntry("tt_eager/tests/ops/test_softmax_op", "ops/test_softmax_op"),
+    # TestEntry(
+    #     "tt_eager/tests/tensors/test_host_device_loopback",
+    #     "tensors/test_host_device_loopback",
+    # ),
+    # TestEntry("tt_eager/tests/tensors/test_copy_and_move", "tensors/test_copy_and_move"),
+    # TestEntry("tt_eager/tests/tensors/test_raw_host_memory_pointer", "tensors/test_raw_host_memory_pointer"),
+    # TestEntry("tt_eager/tests/tensors/test_async_tensor_apis", "tensors/test_async_tensor_apis"),
     # Integration tests
     # void_for_whb0(TestEntry("tt_eager/tests/integration_tests/test_bert", "integration_tests/test_bert")),
 )
