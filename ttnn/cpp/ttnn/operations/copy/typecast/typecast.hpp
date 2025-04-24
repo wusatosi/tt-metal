ef71f9338766c096db193d1048f6b8c3206a575d
KalaivaniMCW <kbaskar@tenstorrent.com>
2025-04-24 16:14:01 +0000

    21127: typecast device op with sub_core_grids

--- ttnn/cpp/ttnn/operations/copy/typecast/typecast.hpp
+++ ttnn/cpp/ttnn/operations/copy/typecast/typecast.hpp
@@ -19,6 +19,17 @@ struct Typecast {
         const std::optional<Tensor>& optional_output_tensor = std::nullopt,
         const std::optional<CoreRangeSet>& sub_core_grids = std::nullopt);
 
+<<<<<<< HEAD
+=======
+    // eltwise_typecast implementation in tt_eager :
+    // ---------------------------------------------
+    // inline Tensor eltwise_typecast(
+    //     const Tensor& input_tensor,
+    //     uint32_t tt_input_dtype,
+    //     uint32_t tt_output_dtype,
+    //     const MemoryConfig& output_mem_config = tt::tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG)
+
+>>>>>>> 555dc22387 (21127: typecast device op with sub_core_grids)
     static ttnn::Tensor invoke(
         const QueueId queue_id,
         const Tensor& input_tensor,
@@ -31,6 +42,11 @@ struct Typecast {
 }  // namespace copy
 }  // namespace operations
 
+<<<<<<< HEAD
 constexpr auto typecast = ttnn::register_operation<"ttnn::typecast", ttnn::operations::copy::Typecast>();
+=======
+constexpr auto typecast =
+    ttnn::register_operation_with_auto_launch_op<"ttnn::typecast", ttnn::operations::copy::Typecast>();
+>>>>>>> 555dc22387 (21127: typecast device op with sub_core_grids)
 
 }  // namespace ttnn
