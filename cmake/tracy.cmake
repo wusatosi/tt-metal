c4856a0c57d5415d0e5d93c83ddd73315e944e5b
Andrew Fuller <afuller@tenstorrent.com>
2025-04-26 01:35:35 +0000

    Define Profiler build type

--- cmake/tracy.cmake
+++ cmake/tracy.cmake
@@ -1,15 +1,41 @@
+get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+if(isMultiConfig)
+    if(NOT "Profiler" IN_LIST CMAKE_CONFIGURATION_TYPES)
+        list(APPEND CMAKE_CONFIGURATION_TYPES Profiler)
+    endif()
+    if(NOT "ProfilerWithDebInfo" IN_LIST CMAKE_CONFIGURATION_TYPES)
+        list(APPEND CMAKE_CONFIGURATION_TYPES ProfilerWithDebInfo)
+    endif()
+endif()
+
+set_property(
+    GLOBAL
+    APPEND
+    PROPERTY
+        DEBUG_CONFIGURATIONS
+            ProfilerWithDebInfo
+)
+
+set(CMAKE_C_FLAGS_PROFILER "${CMAKE_C_FLAGS_RELEASE} -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS_PROFILER "${CMAKE_CXX_FLAGS_RELEASE} -fno-omit-frame-pointer")
+set(CMAKE_EXE_LINKER_FLAGS_PROFILER "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -rdynamic")
+set(CMAKE_MODULE_LINKER_FLAGS_PROFILER "${CMAKE_MODULE_LINKER_FLAGS_RELEASE} -rdynamic")
+set(CMAKE_SHARED_LINKER_FLAGS_PROFILER "${CMAKE_SHARED_LINKER_FLAGS_RELEASE} -rdynamic")
+set(CMAKE_STATIC_LINKER_FLAGS_PROFILER "${CMAKE_STATIC_LINKER_FLAGS_RELEASE} -rdynamic")
+
+# Mimic RelWithDebInfo, but avoid -DNDEBUG to keep TT_ASSERT.  Take Debug and increase the -O level.
+set(CMAKE_C_FLAGS_PROFILERWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS_PROFILERWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -fno-omit-frame-pointer")
+set(CMAKE_EXE_LINKER_FLAGS_PROFILERWITHDEBINFO "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} -rdynamic")
+set(CMAKE_MODULE_LINKER_FLAGS_PROFILERWITHDEBINFO "${CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO} -rdynamic")
+set(CMAKE_SHARED_LINKER_FLAGS_PROFILERWITHDEBINFO "${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO} -rdynamic")
+set(CMAKE_STATIC_LINKER_FLAGS_PROFILERWITHDEBINFO "${CMAKE_STATIC_LINKER_FLAGS_RELWITHDEBINFO} -rdynamic")
+
 # Built as outlined in Tracy documentation (pg.12)
 set(TRACY_HOME ${PROJECT_SOURCE_DIR}/tt_metal/third_party/tracy)
 
-if(NOT ENABLE_TRACY)
-    # Stub Tracy::TracyClient to provide the headers which themselves provide stubs
-    add_library(TracyClient INTERFACE)
-    add_library(Tracy::TracyClient ALIAS TracyClient)
-    target_include_directories(TracyClient SYSTEM INTERFACE "$<BUILD_INTERFACE:${TRACY_HOME}/public>")
-    return()
-endif()
-
-add_subdirectory(${TRACY_HOME})
+set(TRACY_ENABLE OFF) # We'll enable it conditionally
+add_subdirectory(${TRACY_HOME} EXCLUDE_FROM_ALL SYSTEM)
 
 set_target_properties(
     TracyClient
@@ -24,9 +50,16 @@ set_target_properties(
             "tracy"
 )
 
-target_compile_definitions(TracyClient PUBLIC TRACY_ENABLE)
-target_compile_options(TracyClient PUBLIC -fno-omit-frame-pointer)
-target_link_options(TracyClient PUBLIC -rdynamic)
+set(USE_TRACY "$<OR:$<CONFIG:Profiler,ProfilerWithDebInfo>,$<BOOL:${ENABLE_TRACY}>>") # Keep ENABLE_TRACY for backwards compatibility for now
+target_compile_definitions(TracyClient PUBLIC
+  "$<${USE_TRACY}:TRACY_ENABLE>"
+)
+target_compile_options(TracyClient PUBLIC
+  "$<$<BOOL:${ENABLE_TRACY}>:-fno-omit-frame-pointer>" # Backwards compatibility
+)
+target_link_options(TracyClient PUBLIC
+  "$<$<BOOL:${ENABLE_TRACY}>:-rdynamic>" # Backwards compatibility
+)
 
 # Our current fork of tracy does not have CMake support for these subdirectories
 # Once we update, we can change this
@@ -44,10 +77,18 @@ ExternalProject_Add(
     CONFIGURE_COMMAND
         ""
     INSTALL_COMMAND
-        cp ${TRACY_HOME}/csvexport/build/unix/csvexport-release .
+        bash -c "$<${USE_TRACY}:
+          cp ${TRACY_HOME}/csvexport/build/unix/csvexport-release .
+        >"
     BUILD_COMMAND
-        cd ${TRACY_HOME}/csvexport/build/unix && CXX=g++ TRACY_NO_LTO=1 make -f
-        ${TRACY_HOME}/csvexport/build/unix/Makefile
+        bash -c "
+          do_it() {   
+            cd ${TRACY_HOME}/csvexport/build/unix &&
+            CXX=g++ TRACY_NO_LTO=1 make -f ${TRACY_HOME}/csvexport/build/unix/Makefile
+            $<SEMICOLON>
+          } &&
+          $<IF:${USE_TRACY},do_it,true>
+        "
 )
 ExternalProject_Add(
     tracy_capture_tools
@@ -62,9 +103,19 @@ ExternalProject_Add(
     CONFIGURE_COMMAND
         ""
     INSTALL_COMMAND
-        cp ${TRACY_HOME}/capture/build/unix/capture-release .
+        bash -c " $<${USE_TRACY}:
+          cp ${TRACY_HOME}/capture/build/unix/capture-release .
+        >"
     BUILD_COMMAND
-        cd ${TRACY_HOME}/capture/build/unix && CXX=g++ TRACY_NO_LTO=1 make -f ${TRACY_HOME}/capture/build/unix/Makefile
+        bash -c "
+          do_it() {   
+            cd ${TRACY_HOME}/capture/build/unix && 
+            CXX=g++ TRACY_NO_LTO=1 make -f ${TRACY_HOME}/capture/build/unix/Makefile
+            $<SEMICOLON>
+          } &&
+          $<IF:${USE_TRACY},do_it,true>
+        "
+
 )
 add_custom_target(
     tracy_tools
