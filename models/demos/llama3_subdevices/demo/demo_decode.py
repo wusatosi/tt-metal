3db8ba62a4aa7014d7ad6118835776f16ae8afbb
Johanna Rock <jrock@tenstorrent.com>
2025-04-29 13:53:12 +0000

    Merged sampling and typecast commit

--- models/demos/llama3_subdevices/demo/demo_decode.py
+++ models/demos/llama3_subdevices/demo/demo_decode.py
@@ -304,20 +304,7 @@ def run_llama3_demo(
         logger.info(f"tt_out done")
 
         # Sampling
-        # tt_out_tok_reset = tt_sampling(tt_out[0], tt_out_tok)
-
-        # Note: Persistent output buffer used, do not deallocate output!
-        tt_out_gathered = tt_model.tt_ccl.line_all_gather(
-            tt_out[0], dim=3, num_links=2, cluster_axis=0, memory_config=ttnn.DRAM_MEMORY_CONFIG, buffer_key="SAMPLING"
-        )
-        tt_out_rm = ttnn.untilize(tt_out_gathered, use_multicore=True, sub_core_grids=sub_core_grids)
-        # Run argmax only for user0
-        tt_out_rm = ttnn.reshape(
-            tt_out_rm, (1, 1, 1, tt_out_rm.shape[3]), (1, 1, tt_out_rm.shape[2], tt_out_rm.shape[3])
-        )
-        _ = ttnn.argmax(
-            tt_out_rm, dim=3, keepdim=True, use_multicore=True, output_tensor=tt_out_tok, sub_core_grids=sub_core_grids
-        )
+        tt_out_tok = tt_sampling(tt_out[0])
         logger.info(f"sampling done")
 
     if not stress_test:
@@ -349,16 +336,7 @@ def run_llama3_demo(
         mode="decode",
         page_table=page_table_tt,
     )
-
-    # Note: Persistent output buffer used, do not deallocate output!
-    tt_out_gathered = tt_model.tt_ccl.line_all_gather(
-        tt_out[0], dim=3, num_links=2, cluster_axis=0, memory_config=ttnn.DRAM_MEMORY_CONFIG, buffer_key="SAMPLING"
-    )
-    tt_out_rm = ttnn.untilize(tt_out_gathered, use_multicore=True, sub_core_grids=sub_core_grids)
-    tt_out_rm = ttnn.reshape(tt_out_rm, (1, 1, 1, tt_out_rm.shape[3]), (1, 1, tt_out_rm.shape[2], tt_out_rm.shape[3]))
-    _ = ttnn.argmax(
-        tt_out_rm, dim=3, keepdim=True, use_multicore=True, output_tensor=tt_out_tok, sub_core_grids=sub_core_grids
-    )
+    tt_out_tok = tt_sampling(tt_out[0])
 
     if not stress_test:
         ttnn.plus_one(
---
+++ models/demos/llama3_subdevices/tt/add_indices.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+from models.common.lightweightmodule import LightweightModule
+
+
+class TTAddIndices(LightweightModule):
+    def __init__(
+        self,
+        args,
+        mesh_device,
+    ):
+        super().__init__()
+        self.args = args
+        self.mesh_device = mesh_device
+
+        # Create indices tensor
+        num_local_top_k = 32
+        self.indices_device_offsets = torch.ones(
+            1, 1, 32, num_local_top_k * self.args.cluster_shape[0], dtype=torch.int64
+        )
+        per_device_vocab_size = self.args.padded_vocab_size // self.args.cluster_shape[0]
+        per_device_vocab_size = 5000
+        for device_id in range(self.args.cluster_shape[0]):
+            self.indices_device_offsets[:, :, :, device_id * num_local_top_k : (device_id + 1) * num_local_top_k] = (
+                device_id * per_device_vocab_size
+            )
+        self.tt_indices_device_offsets_int32 = ttnn.from_torch(
+            self.indices_device_offsets,
+            device=self.mesh_device,
+            dtype=ttnn.int32,
+            layout=ttnn.TILE_LAYOUT,
+            mesh_mapper=ttnn.ReplicateTensorToMesh(self.mesh_device),
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        )
+
+        self.sub_core_grids = ttnn.CoreRangeSet(
+            [
+                ttnn.CoreRange(ttnn.CoreCoord(1, 0), ttnn.CoreCoord(3, 9)),
+                ttnn.CoreRange(ttnn.CoreCoord(5, 0), ttnn.CoreCoord(6, 9)),
+            ]
+        )
+
+    def forward(self, x: ttnn.Tensor):
+        x = ttnn.typecast(x, dtype=ttnn.uint32, sub_core_grids=self.sub_core_grids)
+        x = ttnn.typecast(x, dtype=ttnn.int32, sub_core_grids=self.sub_core_grids)
+
+        topk_indices_gathered_sharded = ttnn.to_memory_config(x, self.args.model_config["DECODE_SAMPLING_INPUT_MEMCFG"])
+
+        topk_global_indices_int32 = ttnn.add(
+            self.tt_indices_device_offsets_int32, topk_indices_gathered_sharded, dtype=ttnn.int32
+        )
+        ttnn.deallocate(topk_indices_gathered_sharded)
+
+        return topk_global_indices_int32
