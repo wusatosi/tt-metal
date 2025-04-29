3dca4bc1a935a9410d482d3dff19239cce010ce1
gwnagTT <gwang@tenstorrent.com>
2025-04-29 18:38:29 +0000

    Fix the call to `chunked_scaled_dot_product_attention` kernel by removing unsupported input arguments; now 4K image can be run.

--- models/tt_transformers/tt/attention.py
+++ models/tt_transformers/tt/attention.py
@@ -859,8 +859,6 @@ class Attention(LightweightModule):
                 values_BKSD,
                 page_table,
                 chunk_start_idx,
-                is_causal=self.causal_mask,
-                attn_mask=mask,
                 scale=self.scale,
                 compute_kernel_config=self.sdpa_prefill_compute_kernel_cfg,
                 program_config=self.model_config["SDPA_PROGCFG"](seq_len),
