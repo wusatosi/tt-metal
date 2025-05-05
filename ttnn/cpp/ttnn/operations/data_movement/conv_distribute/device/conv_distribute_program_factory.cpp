a80eca499964f09776935f122f34cf9b43a54688
Ilija Anastasijevic <ianastasijevic@tenstorrent.com>
2025-05-05 19:38:07 +0000

    Blocks per core calculation

--- ttnn/cpp/ttnn/operations/data_movement/conv_distribute/device/conv_distribute_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/conv_distribute/device/conv_distribute_program_factory.cpp
@@ -26,8 +26,44 @@ namespace ttnn::operations::data_movement::detail {
 
 operation::ProgramWithCallbacks conv_distribute_multi_core(
     const Tensor& input_tensor, const Tensor& output_tensor, const ttnn::CoreRangeSet& cores, int divisor) {
-    // TODO: the hard part
     tt::tt_metal::Program program{};
+
+    tt::DataFormat input_cb_data_format = tt::tt_metal::datatype_to_dataformat_converter(input_tensor.get_dtype());
+    tt::DataFormat output_cb_data_format = tt::tt_metal::datatype_to_dataformat_converter(output_tensor.get_dtype());
+
+    uint32_t nhw = input_tensor.logical_shape()[2];
+    uint32_t c = input_tensor.logical_shape()[3];
+
+    uint32_t num_cores = cores.num_cores();
+
+    // TODO: less confusing nomenclature?
+    uint32_t evenly_divisible_blocks = nhw / num_cores / divisor;
+    uint32_t evenly_divisible_extra_rows = nhw / num_cores % divisor;
+    uint32_t remainder_blocks = nhw % num_cores / divisor;
+    uint32_t remainder_extra_rows = nhw % num_cores % divisor;
+
+    uint32_t total_extra_blocks =
+        remainder_blocks + (evenly_divisible_extra_rows * num_cores + remainder_extra_rows) / divisor;
+
+    // could not prove this is zero so we calculate it to cover a potential edge case
+    uint32_t evenly_divisible_extra_blocks = total_extra_blocks / num_cores;
+    uint32_t remainder_extra_blocks = total_extra_blocks % num_cores;
+
+    uint32_t num_blocks_per_core = evenly_divisible_blocks + evenly_divisible_extra_blocks;
+    uint32_t num_cores_with_extra_block = remainder_extra_blocks;
+
+    log_info(
+        tt::LogOp,
+        "Num cores: {} num_blocks_per_core: {} num_cores_with_extra_block: {}",
+        num_cores,
+        num_blocks_per_core,
+        num_cores_with_extra_block);
+
+    // TODO: process input and output shard specs
+
+    // TODO: set up circular buffers
+
+    // TODO: create kernels and distribute work to cores
 
     return {.program = std::move(program), .override_runtime_arguments_callback = nullptr};
 }
