1de42c8c41356847690db33cbf323e83f5aec4e6
Nilaykumar Patel <nkpatel@tenstorrent.com>
2025-05-05 10:21:12 +0000

    Add double buffer and split work between reader and write cores.
    Signed-off-by: Nilaykumar Patel <nkpatel@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/conv/conv2dmatmul/device/conv2dmatmul_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2dmatmul/device/conv2dmatmul_program_factory.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "ttnn/operations/conv/conv2dmatmul/device/conv2dmatmul_program_factory.hpp"
+#include "hostdevcommon/kernel_structs.h"
 #include "ttnn/tensor/host_buffer/functions.hpp"
 #include <tt-metalium/work_split.hpp>
 #include "ttnn/operations/math.hpp"
@@ -28,7 +29,6 @@ tt::tt_metal::operation::ProgramWithCallbacks conv2dmatmul_tile_reader_writer(
     const uint32_t input_width,
     std::array<uint32_t, 2> kernel_size,
     std::array<uint32_t, 2> stride) {
-    // This should allocate a DRAM buffer on the device
     auto device = input_tensor.device();
     auto program = tt::tt_metal::CreateProgram();
 
@@ -48,19 +48,24 @@ tt::tt_metal::operation::ProgramWithCallbacks conv2dmatmul_tile_reader_writer(
     tt::log_info("single_tile_size: {}", single_tile_size);
     tt::log_info("input_tensor_shape: {}", input_padded_shape);
     tt::log_info("output_tensor_shape: {}", output_padded_shape);
+
     auto stick_nbytes = output_padded_shape[3] * 2;
     uint32_t ntiles_per_row = tt::div_up(input_padded_shape[-1], TILE_WIDTH);
+
     std::cout << "ntiles_per_row: " << ntiles_per_row << std::endl;
-    uint32_t src0_cb_index = 0;
+    uint32_t src0_cb_index = tt::CBIndex::c_0;
     uint32_t num_input_tiles = ntiles_per_row;
+    uint32_t double_buffer = 2;
     tt::tt_metal::CircularBufferConfig cb_src0_config =
-        tt::tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{src0_cb_index, cb_data_format}})
+        tt::tt_metal::CircularBufferConfig(
+            num_input_tiles * single_tile_size * double_buffer, {{src0_cb_index, cb_data_format}})
             .set_page_size(src0_cb_index, single_tile_size);
     auto cb_src0 = tt::tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
 
-    uint32_t src1_cb_index = 1;  // For output buffer
+    uint32_t src1_cb_index = tt::CBIndex::c_1;
     tt::tt_metal::CircularBufferConfig cb_src1_config =
-        tt::tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{src1_cb_index, cb_data_format}})
+        tt::tt_metal::CircularBufferConfig(
+            num_input_tiles * single_tile_size * double_buffer, {{src1_cb_index, cb_data_format}})
             .set_page_size(src1_cb_index, single_tile_size);
     auto cb_src1 = tt::tt_metal::CreateCircularBuffer(program, core, cb_src1_config);
 
@@ -70,41 +75,32 @@ tt::tt_metal::operation::ProgramWithCallbacks conv2dmatmul_tile_reader_writer(
 
     const std::array reader_kernel_args = {
         src0_buffer->address(),
-        dst_buffer->address(),
-        batch_size,
-        input_height,
-        input_width,
-        kernel_size[0],
-        kernel_size[1],
-        stick_nbytes,
-        std::uint32_t{0},
-        num_unpadded_tiles,
-        ntiles_per_row,
     };
     const std::array writer_kernel_args = {
         dst_buffer->address(),
-        input_width,
-        input_height,
-        stick_nbytes,
-        std::uint32_t{0},
     };
 
-    const std::array compute_kernel_args = {
+    bool src0_is_dram = src0_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
+    bool dst_is_dram = dst_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
+    std::vector<uint32_t> reader_compile_time_args = {
+        std::uint32_t{0},
         num_unpadded_tiles,
         ntiles_per_row,
         src0_cb_index,
+    };
+    std::vector<uint32_t> writer_compile_time_args = {
+        batch_size,
+        input_height,
+        input_width,
+        stride[0],
+        stride[1],
+        stick_nbytes,
+        std::uint32_t{0},
+        num_unpadded_tiles,
+        ntiles_per_row,
+        datum_size(cb_data_format),
         src1_cb_index,
     };
-    // Reader compile-time args
-    // Data is 32 byte aligned
-    bool src0_is_dram = src0_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
-    bool dst_is_dram = dst_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
-    std::vector<uint32_t> reader_compile_time_args = {// interleaved accessor args
-                                                      (std::uint32_t)src0_is_dram};
-    std::vector<uint32_t> writer_compile_time_args = {// interleaved accessor args
-                                                      (std::uint32_t)src0_cb_index,
-                                                      (std::uint32_t)src1_cb_index,
-                                                      (std::uint32_t)dst_is_dram};
     // Tilized reader
     tt::tt_metal::KernelHandle unary_reader_kernel_id = tt::tt_metal::CreateKernel(
         program,
@@ -140,8 +136,6 @@ tt::tt_metal::operation::ProgramWithCallbacks conv2dmatmul_tile_reader_writer(
     tt::tt_metal::SetRuntimeArgs(program, unary_reader_kernel_id, core, reader_kernel_args);
 
     tt::tt_metal::SetRuntimeArgs(program, unary_writer_kernel_id, core, writer_kernel_args);
-
-    tt::tt_metal::SetRuntimeArgs(program, compute_kernel_id, core, compute_kernel_args);
 
     auto override_runtime_args_callback =
         [unary_reader_kernel_id, unary_writer_kernel_id, compute_kernel_id](
