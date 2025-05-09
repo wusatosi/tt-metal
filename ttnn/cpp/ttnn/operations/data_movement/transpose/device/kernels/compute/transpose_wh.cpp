6494c25cd5c649a04824d1b37d0b167353326523
Saad Jameel <sjameel@tenstorrent.com>
2025-05-09 19:51:11 +0000

    add tests for print

--- ttnn/cpp/ttnn/operations/data_movement/transpose/device/kernels/compute/transpose_wh.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/transpose/device/kernels/compute/transpose_wh.cpp
@@ -5,6 +5,8 @@
 #include <cstdint>
 
 #include "compute_kernel_api/transpose_wh.h"
+#include "tt_metal/hw/inc/debug/dprint_pages.h"
+#include "tt_metal/hw/inc/debug/dprint_tensix.h"
 
 namespace NAMESPACE {
 void MAIN {
@@ -16,14 +18,24 @@ void MAIN {
     // - assumes the tiles come in in column major order from reader
     // - uses reader_unary_transpose_wh
     // - transpose_wh each tile
+    UNPACK(DPRINT << "NHtWt " << NHtWt << ENDL());
     for (uint32_t n = 0; n < NHtWt; n++) {
         cb_wait_front(tt::CBIndex::c_0, 1);
         cb_reserve_back(tt::CBIndex::c_16, 1);
 
+        UNPACK(DPRINT << "It " << n << ENDL());
+        UNPACK(tt::compute::common::print_full_tile(tt::CBIndex::c_0, 0, true));
+        UNPACK(DPRINT << ENDL());
+
         acquire_dst();
         transpose_wh_tile(tt::CBIndex::c_0, 0, 0);
+        dprint_tensix_dest_reg(0);
         pack_tile(0, tt::CBIndex::c_16);
         release_dst();
+
+        // PACK(DPRINT << "It " << n << ENDL());
+        // PACK(tt::compute::common::print_full_tile(tt::CBIndex::c_16, 0, true));
+        // PACK(DPRINT << ENDL());
 
         cb_push_back(tt::CBIndex::c_16, 1);
         cb_pop_front(tt::CBIndex::c_0, 1);
