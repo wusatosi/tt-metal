1266678ae33b1fca571cb45f9f55b9197ad2ab9b
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-05-12 13:02:02 +0200

    use TT-NN RMS norm with increased quality settings

--- models/experimental/stable_diffusion3/tt/normalization.py
+++ models/experimental/stable_diffusion3/tt/normalization.py
@@ -39,7 +39,7 @@ class TtRmsNorm:
         parameters: TtRmsNormParameters,
         *,
         eps: float,
-        use_cpu_fallback: bool = True,
+        use_cpu_fallback: bool = False,
     ) -> None:
         super().__init__()
 
@@ -78,7 +78,17 @@ class TtRmsNorm:
                 dtype=ttnn.bfloat16,
             )
 
-        output = ttnn.rms_norm(x, weight=self._weight, epsilon=self._eps)
+        output = ttnn.rms_norm(
+            x,
+            weight=self._weight,
+            epsilon=self._eps,
+            compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                math_fidelity=ttnn.MathFidelity.HiFi4,
+                math_approx_mode=False,
+                fp32_dest_acc_en=True,
+                packer_l1_acc=True,
+            ),
+        )
 
         if deallocate:
             ttnn.deallocate(x)
