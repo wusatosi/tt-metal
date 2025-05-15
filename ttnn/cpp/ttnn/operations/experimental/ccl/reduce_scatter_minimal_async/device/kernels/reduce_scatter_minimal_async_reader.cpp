4046fb853d776a100a4a8fe00375fbfd4d628bee
Colman Glagovich <cglagovich@tenstorrent.com>
2025-05-15 18:26:25 +0000

    Change RT to CT args

--- ttnn/cpp/ttnn/operations/experimental/ccl/reduce_scatter_minimal_async/device/kernels/reduce_scatter_minimal_async_reader.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/reduce_scatter_minimal_async/device/kernels/reduce_scatter_minimal_async_reader.cpp
@@ -24,6 +24,10 @@ constexpr uint32_t cb_intermediate_id = get_compile_time_arg_val(4);
 constexpr uint32_t cb_reader_output_id = get_compile_time_arg_val(5);
 constexpr uint32_t packet_size_in_pages = get_compile_time_arg_val(6);
 constexpr uint32_t input_tensor_page_size = get_compile_time_arg_val(7);
+constexpr uint32_t input_tensor_Wt = get_compile_time_arg_val(8);
+constexpr uint32_t slice_num_pages = get_compile_time_arg_val(9);
+constexpr uint32_t ring_size = get_compile_time_arg_val(10);
+constexpr uint32_t num_batches = get_compile_time_arg_val(11);
 
 void kernel_main() {
     ///////////////////////////////////////////////////
@@ -34,18 +38,14 @@ void kernel_main() {
     // Load the input tensor spec
     address_t input_tensor_address = get_arg_val<address_t>(arg_idx++);
     address_t intermediate_tensor_address = get_arg_val<address_t>(arg_idx++);
-    uint32_t input_tensor_Wt = get_arg_val<uint32_t>(arg_idx++);
-    uint32_t slice_num_pages = get_arg_val<uint32_t>(arg_idx++);
-    uint32_t ring_size = get_arg_val<uint32_t>(arg_idx++);
     size_t out_ready_sem = get_arg_val<uint32_t>(arg_idx++);
     size_t batch_ready_sem = get_arg_val<uint32_t>(arg_idx++);
-    uint32_t num_batches = get_arg_val<uint32_t>(arg_idx++);
 
-    uint32_t batch_slice_num_pages = slice_num_pages / num_batches;
+    constexpr uint32_t batch_slice_num_pages = slice_num_pages / num_batches;
 
-    uint32_t slice_Wt = input_tensor_Wt / ring_size;
+    constexpr uint32_t slice_Wt = input_tensor_Wt / ring_size;
 
-    uint32_t batch_num_pages = slice_num_pages * ring_size / num_batches;
+    constexpr uint32_t batch_num_pages = slice_num_pages * ring_size / num_batches;
 
     constexpr bool input_tensor_is_dram = input_buffer_type == tt::tt_metal::BufferType::DRAM;
     auto input_tensor_addrgen = InterleavedAddrGenFast<input_tensor_is_dram>{
