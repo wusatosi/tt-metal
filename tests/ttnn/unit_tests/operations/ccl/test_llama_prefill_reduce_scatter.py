eb220deb6a86f90b9775aa9177b9308fcb69420c
mvasiljevicTT <mvasiljevic@tenstorrent.com>
2025-05-15 19:30:58 +0000

    wip

---
+++ tests/ttnn/unit_tests/operations/ccl/test_llama_prefill_reduce_scatter.py
@@ -0,0 +1,376 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import pytest
+from loguru import logger
+import ttnn
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal, comp_pcc
+from tests.ttnn.utils_for_testing import assert_with_pcc
+
+from models.utility_functions import skip_for_grayskull
+from models.perf.benchmarking_utils import BenchmarkData, BenchmarkProfiler
+
+from tests.ttnn.unit_tests.operations.ccl.test_new_all_reduce import (
+    SUB_DEVICE_CRS,
+    QKV_CRS,
+    RING_CRS,
+    FF1_CRS,
+    FF1_CRS_RS_OUT,
+    NORM_CRS,
+)
+from tracy import signpost
+from models.demos.llama3_subdevices.tt.llama_common import (
+    check_mesh_tensor_alloc,
+)
+
+PACKET_WORKER_CRS = ttnn.CoreRangeSet(
+    [
+        ttnn.CoreRange(ttnn.CoreCoord(1, 0), ttnn.CoreCoord(3, 1)),
+        ttnn.CoreRange(ttnn.CoreCoord(1, 2), ttnn.CoreCoord(2, 2)),
+    ]
+)
+
+
+def gen_tensor(dim, shard_height, shard_width, num_devices_scatter, num_devices_fracture, num_cores, scheme="random"):
+    factor = 0
+    torch_fracture_tensors = []
+    for _ in range(num_devices_fracture):
+        torch_scatter_tensors = []
+        for _ in range(num_devices_scatter):
+            torch_input_tensors = []
+            for _ in range(num_cores):
+                for _ in range(shard_width // 32):
+                    if scheme == "random":
+                        torch_input_tensors.append(torch.rand(1, 1, shard_height, 32))
+                    elif scheme == "sequential":
+                        torch_input_tensors.append(torch.ones(1, 1, shard_height, 32) * factor)
+                        factor += 1
+                    else:
+                        raise ValueError(f"Invalid scheme: {scheme}")
+            torch_scatter_tensors.append(torch.cat(torch_input_tensors, dim=dim))
+
+        torch_fracture_tensors.append(torch.cat(torch_scatter_tensors, dim=1))
+
+    return torch.cat(torch_fracture_tensors, dim=0)
+
+
+def run_reduce_scatter_test(
+    mesh_device,
+    dim,
+    shard_height,
+    shard_width,
+    num_devices_scatter,
+    num_devices_fracture,
+    num_cores,
+    num_iters,
+    warmup_iters,
+    trace_mode,
+    num_links=3,
+    scheme="random",
+    use_regular_grid=False,
+    input_grid=None,
+    output_grid=None,
+    dtype=ttnn.bfloat8_b,
+    profiler=BenchmarkProfiler(),
+):
+    mesh_device.enable_program_cache()
+    num_pages_per_packet = 4
+    cyclic_buffer_size = 8
+
+    # input, output, interm core range set
+    # compute_grid = (mesh_device.compute_with_storage_grid_size().x, mesh_device.compute_with_storage_grid_size().y)
+    compute_grid = (4, 2)
+    import pdb
+
+    pdb.set_trace()
+    subdevice_shard_cores_grid = ttnn.CoreRangeSet(
+        {
+            ttnn.CoreRange(
+                ttnn.CoreCoord(0, 0),
+                ttnn.CoreCoord(compute_grid[0] - 1, compute_grid[1] - 1),
+            ),
+        }
+    )
+    if input_grid is not None:
+        input_shard_cores_grid = ttnn.CoreRangeSet(
+            {
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(0, 0),
+                    ttnn.CoreCoord(input_grid[0] - 1, input_grid[1] - 1),
+                ),
+            }
+        )
+    if output_grid is not None:
+        output_shard_cores_grid = ttnn.CoreRangeSet(
+            {
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(0, 0),
+                    ttnn.CoreCoord(output_grid[0] - 1, output_grid[1] - 1),
+                ),
+            }
+        )
+        tensor_width_in_tiles = num_cores * shard_width
+        output_num_cores = output_grid[0] * output_grid[1]
+
+    # input, output, interm memory config
+    sharded_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            input_shard_cores_grid if use_regular_grid else RING_CRS,
+            [shard_height, shard_width],
+            ttnn.ShardOrientation.ROW_MAJOR,
+        ),
+    )
+    packet_workers_persistent_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            subdevice_shard_cores_grid if use_regular_grid else SUB_DEVICE_CRS,
+            [shard_height, num_devices_scatter * num_pages_per_packet * 32],
+            ttnn.ShardOrientation.ROW_MAJOR,
+        ),
+    )
+    output_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            output_shard_cores_grid if use_regular_grid else FF1_CRS_RS_OUT,
+            [
+                shard_height,
+                tensor_width_in_tiles // output_num_cores // num_devices_scatter if use_regular_grid else 32,
+            ],
+            ttnn.ShardOrientation.ROW_MAJOR,
+        ),
+    )
+
+    output_tensor_goldens_list = []
+    tt_input_tensors_list = []
+    tt_intermediate_tensors_list = []
+    for iter in range(num_iters):
+        input = gen_tensor(
+            dim, shard_height, shard_width, num_devices_scatter, num_devices_fracture, num_cores, scheme=scheme
+        )
+
+        intermediate_tensor = torch.zeros(
+            [
+                num_devices_fracture,
+                num_devices_scatter,
+                shard_height,
+                num_devices_scatter
+                * num_pages_per_packet
+                * 32
+                * packet_workers_persistent_mem_config.shard_spec.num_cores(),
+            ]
+        )
+
+        intermediate_outputs = torch.chunk(input, chunks=num_devices_scatter, dim=1)
+        output = torch.zeros(intermediate_outputs[0].shape)
+
+        for i in range(0, len(intermediate_outputs)):
+            output += intermediate_outputs[i]
+
+        scattered_output = torch.chunk(output, chunks=num_devices_scatter, dim=dim)
+        scattered_output = torch.cat(scattered_output, dim=1)
+
+        output_tensor_goldens_list.append(scattered_output)
+
+        # import pdb; pdb.set_trace()
+        tt_input = ttnn.from_torch(
+            input,
+            device=mesh_device,
+            layout=ttnn.TILE_LAYOUT,
+            dtype=dtype,
+            memory_config=sharded_mem_config,
+            mesh_mapper=ttnn.ShardTensor2dMesh(
+                mesh_device, dims=(0, 1), mesh_shape=[num_devices_fracture, num_devices_scatter]
+            ),
+        )
+        if iter < cyclic_buffer_size:
+            tt_intermediate = ttnn.from_torch(
+                intermediate_tensor,
+                device=mesh_device,
+                layout=ttnn.TILE_LAYOUT,
+                dtype=dtype,
+                memory_config=packet_workers_persistent_mem_config,
+                mesh_mapper=ttnn.ShardTensor2dMesh(
+                    mesh_device, dims=(0, 1), mesh_shape=[num_devices_fracture, num_devices_scatter]
+                ),
+            )
+            check_mesh_tensor_alloc(tt_intermediate)
+            tt_intermediate_tensors_list.append(tt_intermediate)
+        check_mesh_tensor_alloc(tt_input)
+        tt_input_tensors_list.append(tt_input)
+
+    ccl_sub_device_crs = subdevice_shard_cores_grid if use_regular_grid is not None else SUB_DEVICE_CRS
+    worker_sub_device = ttnn.SubDevice(
+        [
+            ccl_sub_device_crs,
+        ]
+    )
+    worker_sub_device_id = ttnn.SubDeviceId(0)
+    sub_device_stall_group = [worker_sub_device_id]
+    sub_device_manager = mesh_device.create_sub_device_manager([worker_sub_device], 0)
+    mesh_device.load_sub_device_manager(sub_device_manager)
+    mesh_device.set_sub_device_stall_group(sub_device_stall_group)
+
+    # create global semaphore handles
+    ccl_semaphore_handles = [ttnn.create_global_semaphore(mesh_device, ccl_sub_device_crs, 0) for _ in range(num_iters)]
+
+    tt_out_tensor_list = []
+
+    def run_op(n_iters, store_all_results=True):
+        tt_output_list = []
+        for i in range(n_iters):
+            buffer_index = 0 if trace_mode else i
+            tt_out_tensor = ttnn.experimental.llama_reduce_scatter(
+                tt_input_tensors_list[buffer_index],
+                tt_intermediate_tensors_list[buffer_index % cyclic_buffer_size],
+                dim,
+                ccl_semaphore_handles[buffer_index],
+                worker_sub_device_id,
+                cluster_axis=1,
+                mesh_device=mesh_device,
+                num_links=num_links,
+                memory_config=output_mem_config,
+            )
+            if not trace_mode:
+                ttnn.synchronize_device(mesh_device)
+            if store_all_results:
+                tt_output_list.append(tt_out_tensor)
+        if store_all_results:
+            return tt_output_list
+        else:
+            return [tt_out_tensor]
+
+    mesh_device.reset_sub_device_stall_group()
+
+    passed = True
+    first_failed_tensor_index = None
+    failed_indices = []
+    expected_pcc = 0.999 if dtype == ttnn.bfloat8_b else 0.9999
+    for tensor_index in range(len(tt_out_tensor_list)):
+        tt_torch_tensor = ttnn.to_torch(
+            tt_out_tensor_list[tensor_index],
+            mesh_composer=ttnn.ConcatMesh2dToTensor(
+                mesh_device, mesh_shape=[num_devices_fracture, num_devices_scatter], dims=(0, 1)
+            ),
+        )
+        eq, output_results = comp_pcc(tt_torch_tensor, output_tensor_goldens_list[tensor_index], expected_pcc)
+        logger.info(f"Output tensor {tensor_index} has result {output_results}")
+        if not eq:
+            passed = False
+            first_failed_tensor_index = tensor_index
+            failed_indices = torch.where(tt_torch_tensor != output_tensor_goldens_list[tensor_index])
+            break
+
+    logger.info(f"Device has {mesh_device.num_program_cache_entries()} program cache entries")
+    assert (
+        mesh_device.num_program_cache_entries() == 1 or mesh_device.num_program_cache_entries() == num_iters
+    ), f"Device {i} has {mesh_device.num_program_cache_entries()} program cache entries"
+
+    if not passed:
+        logger.info(f"Failed indices: {failed_indices}")
+        assert eq, f"{first_failed_tensor_index} FAILED: {output_results}"
+
+
+@pytest.mark.parametrize(
+    "device_params",
+    [
+        {
+            "trace_region_size": 90000,
+            "dispatch_core_axis": ttnn.DispatchCoreAxis.ROW,
+            "fabric_config": ttnn.FabricConfig.FABRIC_1D,
+        }
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize("trace_mode", [True, False])
+@pytest.mark.parametrize(
+    "mesh_device",
+    [
+        (1, 2),  # TODO: Once fabric can be initialized on a SubMesh, revert to (1, 2)
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize("shard_height", [32])
+@pytest.mark.parametrize("shard_width", [64])
+@pytest.mark.parametrize("input_grid", [(5, 4)])
+@pytest.mark.parametrize("output_grid", [(5, 2)])
+@pytest.mark.parametrize("dtype", [ttnn.bfloat16])
+def test_fabric_reduce_scatter_regular_grid_2_dev(
+    mesh_device, trace_mode, shard_height, shard_width, input_grid, output_grid, dtype
+):
+    # Only run these tests on unharvested TG
+    device_grid = (mesh_device.compute_with_storage_grid_size().x, mesh_device.compute_with_storage_grid_size().y)
+    if device_grid != (8, 8):
+        pytest.skip("Not TG!")
+
+    dim = 3
+    num_devices_scatter = 4
+    num_devices_fracture = 8
+    num_cores = input_grid[0] * input_grid[1]
+    num_iters = 30
+    warmup_iters = 0
+    run_reduce_scatter_test(
+        mesh_device,
+        dim,
+        shard_height,
+        shard_width,
+        num_devices_scatter,
+        num_devices_fracture,
+        num_cores,
+        num_iters,
+        warmup_iters,
+        trace_mode,
+        num_links=1,
+        scheme="random",
+        use_regular_grid=True,
+        input_grid=input_grid,
+        output_grid=output_grid,
+        dtype=dtype,
+    )
+
+
+@pytest.mark.parametrize("mesh_device", [pytest.param((1, 8), id="1x8_grid")], indirect=True)
+@pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_1D_RING}], indirect=True)
+def test_reduce_scatter_on_T3K(mesh_device):
+    torch_tensor = torch.rand((32, 512), dtype=torch.bfloat16)
+    for i in range(16):
+        torch_tensor[:, i * 32 : (i + 1) * 32] = i
+        # torch_tensor[:, i * 32 : (i + 1) * 32] = i
+    # Convert to ttnn.Tensor, tilize and move onto devices across mesh DRAM
+    mesh_tensor = ttnn.from_torch(
+        torch_tensor,
+        layout=ttnn.TILE_LAYOUT,
+        device=mesh_device,
+        mesh_mapper=ttnn.ShardTensorToMesh(mesh_device, dim=1),
+        dtype=ttnn.bfloat8_b,
+    )
+
+    semaphore_crs = ttnn.CoreRangeSet([ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(6, 6))])
+    semaphore_sent = ttnn.create_global_semaphore(mesh_device=mesh_device, cores=semaphore_crs, initial_value=0)
+    semaphore_can_receive = ttnn.create_global_semaphore(mesh_device=mesh_device, cores=semaphore_crs, initial_value=0)
+    semaphores = [semaphore_sent, semaphore_can_receive]
+    # Execute Line All-Gather on the tensor
+    print(mesh_tensor)
+
+    output_tensor = ttnn.experimental.llama_prefill_reduce_scatter(mesh_tensor, semaphores[0])
+
+    # ttnn.set_printoptions(profile="full")
+
+    # for i in range(16):
+    #     j = i * 32
+    #     print("DEBUG_ROW", i, output_tensor[0, j : j + 32])
+    # print(output_tensor)
+
+    mesh_composer = ttnn.ConcatMesh2dToTensor(mesh_device, mesh_shape=(1, 8), dims=[0, 1])
+
+    tt_output_tensor = ttnn.to_torch(output_tensor, mesh_composer=mesh_composer).to(torch_tensor.dtype)
+    # take 1/8 of this
+    tt_output_tensor = tt_output_tensor[:, : torch_tensor.shape[1]]
+
+    print(torch_tensor.shape, tt_output_tensor.shape, tt_output_tensor)
+    # assert torch.allclose(torch_tensor, tt_output_tensor, atol=0.1), "Tensor comparison failed"
---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_prefill_reduce_scatter/device/kernels/dataflow/reader_llama_prefill_reduce_scatter.cpp
@@ -0,0 +1,211 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+#include "dataflow_api.h"
+#include "cpp/ttnn/operations/ccl/shared_with_host/sharded_tensor_addr_gen.hpp"
+#include "ttnn/cpp/ttnn/operations/ccl/kernel_common/sharding_addrgen.hpp"
+// #include <unistd.h>
+
+template <uint8_t noc_ind = noc_index>
+FORCE_INLINE std::uint64_t static_noc_multicast_addr(
+    std::uint32_t noc_x_start,
+    std::uint32_t noc_y_start,
+    std::uint32_t noc_x_end,
+    std::uint32_t noc_y_end,
+    std::uint32_t addr) {
+    if constexpr (noc_ind == 0) {
+        return get_noc_multicast_addr(noc_x_start, noc_y_start, noc_x_end, noc_y_end, addr);
+    } else {
+        return get_noc_multicast_addr(noc_x_end, noc_y_end, noc_x_start, noc_y_start, addr);
+    }
+}
+
+void kernel_main() {
+    // Constants for indexing
+    constexpr uint8_t x_index = 0;
+    constexpr uint8_t y_index = 1;
+
+    size_t rt_arg_idx = 0;
+
+    // Define all compile-time arguments at the beginning
+    constexpr uint32_t input_tensor_cb_id = get_compile_time_arg_val(0);
+    constexpr uint32_t fabric_sender_cb_id = get_compile_time_arg_val(1);
+    constexpr uint32_t packet_header_cb_id = get_compile_time_arg_val(2);
+    constexpr uint32_t fabric_receiver_cb_id = get_compile_time_arg_val(3);
+    constexpr uint32_t accumulator_cb_id = get_compile_time_arg_val(4);
+    constexpr uint32_t output_tensor_cb_id = get_compile_time_arg_val(5);
+    constexpr uint32_t chip_id = get_compile_time_arg_val(6);
+    // constexpr uint32_t tiles_per_core_width = get_compile_time_arg_val(7);
+    // constexpr uint32_t tiles_per_core_width_output = get_compile_time_arg_val(8);
+    // constexpr uint32_t num_pages_per_packet = get_compile_time_arg_val(9);
+    // constexpr uint32_t input_shard_cores_per_device = get_compile_time_arg_val(10);
+    constexpr uint32_t num_devices = get_compile_time_arg_val(7);
+    constexpr uint32_t page_size_bytes = get_compile_time_arg_val(8);
+    // constexpr uint32_t output_cores_per_device = get_compile_time_arg_val(13);
+    // constexpr uint32_t packet_worker_start_x = get_compile_time_arg_val(14);
+    // constexpr uint32_t packet_worker_start_y = get_compile_time_arg_val(15);
+    // constexpr uint32_t packet_worker_end_x = get_compile_time_arg_val(16);
+    // constexpr uint32_t packet_worker_end_y = get_compile_time_arg_val(17);
+    // constexpr uint32_t num_sender_cores = get_compile_time_arg_val(18);
+    // constexpr uint32_t total_num_read_txns = get_compile_time_arg_val(19);
+
+    // Derived compile-time constants
+    // constexpr uint32_t input_tensor_cores = input_shard_cores_per_device * num_devices;
+    // constexpr uint32_t num_packets_total_per_device =
+    //     (input_shard_cores_per_device * tiles_per_core_width + num_pages_per_packet - 1) / num_pages_per_packet;
+
+    // // Precompute constants for optimization
+    // constexpr uint32_t bytes_per_tile_group = tiles_per_core_width * page_size_bytes;
+    // constexpr uint32_t num_dests =
+    //     (packet_worker_end_x - packet_worker_start_x + 1) * (packet_worker_end_y - packet_worker_start_y + 1);
+    // constexpr uint32_t chip_id_offset = chip_id * num_pages_per_packet * page_size_bytes;
+
+    // constexpr uint32_t other_devices = num_devices - 1;
+    // constexpr uint8_t device_order[other_devices] =
+    //     DEVICE_ORDER;  // this is code gen'd in the program factory using the defines
+    // constexpr uint8_t input_core_xy[input_tensor_cores][2] = INPUT_CORE_XY;
+    // constexpr uint8_t output_core_xy[output_cores_per_device][2] = OUTPUT_CORE_XY;
+    // constexpr uint8_t schedule[total_num_read_txns][3] = SCHEDULE;
+    // constexpr uint32_t total_senders = num_sender_cores * other_devices;
+
+    // // Runtime arguments
+    uint32_t receiver_semaphore_address = get_arg_val<uint32_t>(rt_arg_idx++);
+    uint32_t local_semaphore_address = get_semaphore(get_arg_val<uint32_t>(rt_arg_idx++));
+    bool sender_core = (bool)get_arg_val<uint32_t>(rt_arg_idx++);
+    bool worker_core = (bool)get_arg_val<uint32_t>(rt_arg_idx++);
+    uint32_t linear_input_packet_start_idx = get_arg_val<uint32_t>(rt_arg_idx++);
+    bool receiver_core = (bool)get_arg_val<uint32_t>(rt_arg_idx++);
+    uint32_t sender_shard_start = get_arg_val<uint32_t>(rt_arg_idx++);
+    uint32_t sender_shard_end = get_arg_val<uint32_t>(rt_arg_idx++);
+    uint32_t sender_total_num_pages = get_arg_val<uint32_t>(rt_arg_idx++);
+
+    // // Bank base addresses (compute once)
+    // const uint32_t bank_base_address = get_write_ptr(input_tensor_cb_id);
+    uint32_t input_tensor_address = get_write_ptr(input_tensor_cb_id);
+    uint32_t output_tensor_address = get_write_ptr(output_tensor_cb_id);
+
+    volatile tt_l1_ptr uint32_t* signal_semaphore_addr_ptr =
+        reinterpret_cast<volatile tt_l1_ptr uint32_t*>(local_semaphore_address);
+
+    constexpr uint32_t start_tile = 0;
+    constexpr uint32_t end_tile = 1;
+    constexpr uint32_t input_num_tiles = end_tile - start_tile;
+
+    uint32_t sender_read_addr = get_write_ptr(fabric_sender_cb_id);
+    if (sender_core) {
+        DPRINT << "i am reader sender core: " << chip_id << ENDL();
+
+        auto tensor0_addrgen = InterleavedAddrGenFast<true>{
+            .bank_base_address = input_tensor_address,
+            .page_size = page_size_bytes,
+            .data_format = get_dataformat(input_tensor_cb_id)};
+
+        uint32_t tile_id = 0;
+
+        // read from input tensor to input tensor??
+        for (; tile_id < input_num_tiles; tile_id++) {
+            cb_reserve_back(fabric_sender_cb_id, 1);
+            auto tensor_tile_addr = tensor0_addrgen.get_noc_addr(tile_id);
+            noc_async_read(tensor_tile_addr, get_write_ptr(input_tensor_cb_id), page_size_bytes);
+            noc_async_read_barrier();
+            cb_push_back(fabric_sender_cb_id, 1);
+        }
+        DPRINT << "DONE READER SENDER" << ENDL();
+    }
+
+    // for (uint32_t target_device_id : device_order) {
+    //     const uint32_t base_core = target_device_id * input_shard_cores_per_device;
+
+    //     uint32_t num_pages_read = 0;
+    //     uint32_t num_pages_reserve_push = 0;
+    //     uint32_t shard_idx = sender_shard_start;
+    //     while (num_pages_read < sender_total_num_pages) {
+    //         const uint8_t curr_core = base_core + schedule[shard_idx][0];
+    //         const uint32_t read_offset = schedule[shard_idx][1];
+    //         const uint32_t read_size = schedule[shard_idx][2];
+    //         num_pages_reserve_push += read_size;
+
+    //         auto num_pages_left = sender_total_num_pages - num_pages_read;
+    //         const uint32_t curr_packet_num_pages = std::min(num_pages_per_packet, num_pages_left);
+
+    //         const uint32_t x = input_core_xy[curr_core][x_index];
+    //         const uint32_t y = input_core_xy[curr_core][y_index];
+    //         const uint32_t offset_address = bank_base_address + (read_offset * page_size_bytes);
+    //         const uint64_t shard_noc_addr = get_noc_addr(x, y, offset_address);
+    //         const uint32_t transfer_size = read_size * page_size_bytes;
+
+    //         cb_reserve_back(fabric_sender_cb_id, num_pages_reserve_push);
+    //         noc_async_read(shard_noc_addr, sender_read_addr, transfer_size);
+
+    //         if (num_pages_reserve_push >= curr_packet_num_pages) {
+    //             noc_async_read_barrier();
+    //             cb_push_back(fabric_sender_cb_id, num_pages_reserve_push);
+    //             num_pages_reserve_push = 0;
+    //         }
+
+    //         sender_read_addr += transfer_size;
+    //         num_pages_read += read_size;
+    //         shard_idx++;
+    //     }
+    // }
+    // }
+    else if (worker_core) {
+        DPRINT << "i am reader worker core: " << chip_id << ENDL();
+
+        auto output_tensor_addrgen = InterleavedAddrGenFast<true>{
+            .bank_base_address = output_tensor_address,
+            .page_size = page_size_bytes,
+            .data_format = get_dataformat(input_tensor_cb_id)};
+
+        uint32_t tile_id = 0;
+
+        // wait semaphore before reading ??
+        noc_semaphore_wait(signal_semaphore_addr_ptr, 1);
+
+        for (tile_id = start_tile; tile_id < end_tile; tile_id++) {
+            cb_reserve_back(fabric_receiver_cb_id, 1);
+            uint64_t tile_addr = output_tensor_address + tile_id * page_size_bytes;
+            noc_async_read(tile_addr, get_write_ptr(fabric_sender_cb_id), page_size_bytes);
+            noc_async_read_barrier();
+            cb_push_back(fabric_receiver_cb_id, 1);
+        }
+
+        DPRINT << "READER: FINAL semaphore value: "
+               << *reinterpret_cast<volatile tt_l1_ptr uint32_t*>(signal_semaphore_addr_ptr) << "\n";
+
+        DPRINT << "DONE WORKER READER\n";
+
+        // Calculate base addresses once
+        //     const uint32_t base_input_tensor_addr = get_read_ptr(input_tensor_cb_id);
+        //     const uint32_t base_receiver_l1_addresses = get_read_ptr(fabric_receiver_cb_id) + chip_id_offset;
+
+        //     for (uint32_t i = 0; i < num_pages_per_packet; i++) {
+        //         const uint32_t rem = linear_input_packet_start_idx + i;
+        //         const uint32_t linear_input_core_idcs = rem / tiles_per_core_width;
+        //         const uint32_t linear_input_tile_offsets = rem % tiles_per_core_width;
+
+        //         if (linear_input_core_idcs >= input_tensor_cores) {
+        //             break;
+        //         }
+
+        //         const uint32_t core_x = input_core_xy[linear_input_core_idcs][x_index];
+        //         const uint32_t core_y = input_core_xy[linear_input_core_idcs][y_index];
+        //         const uint32_t tile_offset = linear_input_tile_offsets * page_size_bytes;
+
+        //         const uint64_t output_noc_address = get_noc_addr(core_x, core_y, base_input_tensor_addr +
+        //         tile_offset); const uint32_t receiver_l1_address = base_receiver_l1_addresses + i * page_size_bytes;
+
+        //         noc_async_read(output_noc_address, receiver_l1_address, page_size_bytes);
+        //     }
+
+        //     noc_semaphore_wait((uint32_t*)receiver_semaphore_address, other_devices);
+
+        //     noc_async_read_barrier();
+        //     cb_push_back(fabric_receiver_cb_id, num_pages_per_packet * num_devices);
+    }
+    // noc_semaphore_set((uint32_t*)local_semaphore_address, INVALID);
+    // noc_semaphore_set((uint32_t*)receiver_semaphore_address, INVALID);
+    // noc_async_write_barrier();
+}
