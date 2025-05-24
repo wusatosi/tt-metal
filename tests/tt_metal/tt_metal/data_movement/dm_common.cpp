f50dcdc426ed46e7f0475a7a4e5d4edfe5771ee8
Eugene Lee <eugenelee@tenstorrent.com>
2025-05-24 21:31:38 +0000

    To be squashed

--- tests/tt_metal/tt_metal/data_movement/dm_common.cpp
+++ tests/tt_metal/tt_metal/data_movement/dm_common.cpp
@@ -5,5 +5,9 @@
 #include "dm_common.hpp"
 
 namespace tt::tt_metal::unit_tests::dm {
+
 uint32_t runtime_host_id = 0;
-}
+
+uint32_t obtain_page_size_bytes(tt::ARCH arch) { return (arch == tt::ARCH::BLACKHOLE) ? 64 : 32; }
+
+}  // namespace tt::tt_metal::unit_tests::dm
--- tests/tt_metal/tt_metal/data_movement/dm_common.hpp
+++ tests/tt_metal/tt_metal/data_movement/dm_common.hpp
@@ -6,10 +6,15 @@
 #define DM_COMMON_HPP
 
 #include <cstdint>
+#include "device_fixture.hpp"
 
 namespace tt::tt_metal::unit_tests::dm {
 // Unique id for each test run
 extern uint32_t runtime_host_id;
+
+// Function to obtain page size in bytes
+uint32_t obtain_page_size_bytes(tt::ARCH arch);
+
 }  // namespace tt::tt_metal::unit_tests::dm
 
 #endif  // DM_COMMON_HPP
