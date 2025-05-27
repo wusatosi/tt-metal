d41ec3d40b35c1084eccadd009c065172e301e52
Brett Grady <bgrady@tenstorrent.com>
2025-05-27 19:06:15 +0000

    fixed hop counting logic in fabric event profile utils

--- tt_metal/tools/profiler/fabric_event_profile_utils.hpp
+++ tt_metal/tools/profiler/fabric_event_profile_utils.hpp
@@ -19,22 +19,23 @@ int get_routing_hops(uint8_t routing_fields_value) {
     return tt::tt_fabric::RoutingFields::HOP_DISTANCE_MASK & routing_fields_value;
 }
 
-void record_fabric_header(const volatile tt::tt_fabric::PacketHeader* fabric_header_ptr) {
+void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) {
 #ifdef PROFILE_NOC_EVENTS
 
-#if defined FABRIC_LOW_LATENCY_MODE and FABRIC_LOW_LATENCY_MODE == 1
-    auto fh = reinterpret_cast<const volatile tt::tt_fabric::LowLatencyPacketHeader*>(fabric_header_ptr);
-    auto num_hops = get_low_latency_routing_hops(fh->routing_fields.value);
-#else
-    auto fh = reinterpret_cast<const volatile tt::tt_fabric::PacketHeader*>(fabric_header_ptr);
-    auto num_hops = get_routing_hops(fh->routing_fields.value);
-#endif
+    int num_hops = 0;
+    if constexpr (std::is_base_of_v<tt::tt_fabric::LowLatencyRoutingFields, ROUTING_FIELDS_TYPE>) {
+        num_hops = get_low_latency_routing_hops(fabric_header_ptr->routing_fields.value);
+    } else if constexpr (std::is_base_of_v<tt::tt_fabric::RoutingFields, ROUTING_FIELDS_TYPE>) {
+        num_hops = get_routing_hops(fabric_header_ptr->routing_fields.value);
+    } else {
+        num_hops = 0;
+    }
 
-    auto noc_send_type = fh->get_noc_send_type();
+    auto noc_send_type = fabric_header_ptr->get_noc_send_type();
 
     switch (noc_send_type) {
         case tt::tt_fabric::NocSendType::NOC_UNICAST_WRITE: {
-            const volatile auto& unicast_write_cmd = fh->get_command_fields().unicast_write;
+            const volatile auto& unicast_write_cmd = fabric_header_ptr->get_command_fields().unicast_write;
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_WRITE,
                 unicast_write_cmd.noc_address,
@@ -42,7 +43,7 @@ void record_fabric_header(const volatile tt::tt_fabric::PacketHeader* fabric_hea
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_UNICAST_ATOMIC_INC: {
-            const volatile auto& unicast_write_cmd = fh->get_command_fields().unicast_seminc;
+            const volatile auto& unicast_write_cmd = fabric_header_ptr->get_command_fields().unicast_seminc;
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_ATOMIC_INC,
                 unicast_write_cmd.noc_address,
@@ -50,7 +51,7 @@ void record_fabric_header(const volatile tt::tt_fabric::PacketHeader* fabric_hea
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_FUSED_UNICAST_ATOMIC_INC: {
-            const volatile auto& unicast_write_cmd = fh->get_command_fields().unicast_seminc_fused;
+            const volatile auto& unicast_write_cmd = fabric_header_ptr->get_command_fields().unicast_seminc_fused;
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_FUSED_UNICAST_ATOMIC_INC,
                 unicast_write_cmd.noc_address,
@@ -58,7 +59,7 @@ void record_fabric_header(const volatile tt::tt_fabric::PacketHeader* fabric_hea
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_UNICAST_INLINE_WRITE: {
-            const volatile auto& unicast_write_cmd = fh->get_command_fields().unicast_inline_write;
+            const volatile auto& unicast_write_cmd = fabric_header_ptr->get_command_fields().unicast_inline_write;
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_INLINE_WRITE,
                 unicast_write_cmd.noc_address,
@@ -75,8 +76,7 @@ void record_fabric_header(const volatile tt::tt_fabric::PacketHeader* fabric_hea
 }
 }  // namespace kernel_profiler
 
-#define RECORD_FABRIC_HEADER(_fabric_header_ptr)                                                \
-    {                                                                                           \
-        kernel_profiler::record_fabric_header(                                                  \
-            reinterpret_cast<const volatile tt::tt_fabric::PacketHeader*>(_fabric_header_ptr)); \
+#define RECORD_FABRIC_HEADER(_fabric_header_ptr)                   \
+    {                                                              \
+        kernel_profiler::record_fabric_header(_fabric_header_ptr); \
     }
